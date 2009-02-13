44142c27015b98e0684376560addab916c8f6ca5
Nathan Wright <nathan@simplestation.com>
2009-02-13 15:01:12 -0800

    Separate Video model into a utf8 table def and a POPO model

--- mediaplex/model/video.py
+++ mediaplex/model/video.py
@@ -2,27 +2,32 @@ from datetime import datetime
 
 from sqlalchemy import Table, ForeignKey, Column
 from sqlalchemy.types import String, Unicode, UnicodeText, Integer, DateTime, Boolean, Float
-from sqlalchemy.orm import relation, backref, synonym
+from sqlalchemy.orm import mapper, relation, backref, synonym
 
 from mediaplex.model import DeclarativeBase, metadata, DBSession
 
 
-class Video(DeclarativeBase):
-    """Video definition"""
-    __tablename__ = 'videos'
+videos = Table('videos', metadata,
+    Column('id', Integer, autoincrement=True, primary_key=True),
+    Column('slug', String(50), unique=True, nullable=False),
+    Column('title', Unicode(50), nullable=False),
+    Column('url', String(255), nullable=False),
+    Column('length', Integer, nullable=False),
+    Column('date_added', DateTime, default=datetime.now, nullable=False),
+    Column('date_modified', DateTime, default=datetime.now, onupdate=datetime.now, nullable=False),
+    Column('views', Integer, default=0, nullable=False),
+    Column('description', UnicodeText),
+    mysql_engine='InnoDB',
+    mysql_charset='utf8'
+)
 
-    id = Column(Integer, autoincrement=True, primary_key=True)
-    slug = Column(String(50), unique=True, nullable=False)
-    title = Column(Unicode(50), nullable=False)
-    url = Column(String(255), nullable=False)
-    length = Column(Integer, nullable=False)
-    date_added = Column(DateTime, default=datetime.now, nullable=False)
-    date_modified = Column(DateTime, default=datetime.now, onupdate=datetime.now, nullable=False)
-    views = Column(Integer, default=0, nullable=False)
-    description = Column(UnicodeText)
+class Video(object):
+    """Video definition"""
 
     def __repr__(self):
         return '<Video: title=%s>' % self.title
 
     def __unicode__(self):
         return self.title
+
+mapper(Video, videos)
