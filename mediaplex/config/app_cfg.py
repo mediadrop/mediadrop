90750a1079ed15d39772da76a6a15fa184321d83
Nathan Wright <nathan@simplestation.com>
2009-02-17 12:47:35 -0800

    Setup app_cfg for defining named routes

--- mediaplex/config/app_cfg.py
+++ mediaplex/config/app_cfg.py
@@ -1,20 +1,34 @@
-from tg.configuration import AppConfig, Bunch
+from tg.configuration import AppConfig, Bunch, config
+from routes import Mapper
+
 import mediaplex
 from mediaplex import model
 from mediaplex.lib import app_globals, helpers
 
-base_config = AppConfig()
+class MediaplexConfig(AppConfig):
+    def setup_routes(self):
+        """Setup our custom named routes"""
+        map = Mapper(directory=config['pylons.paths']['controllers'],
+                    always_scan=config['debug'])
+
+        # Setup a default route for the root of object dispatch
+        map.connect('*url', controller='root', action='routes_placeholder')
+
+        config['routes.map'] = map
+
+
+base_config = MediaplexConfig()
 base_config.renderers = []
 
 base_config.package = mediaplex
 
 #Set the default renderer
 base_config.default_renderer = 'genshi'
-base_config.renderers.append('genshi') 
+base_config.renderers.append('genshi')
 # if you want raw speed and have installed chameleon.genshi
 # you should try to use this renderer instead.
 # warning: for the moment chameleon does not handle i18n translations
-#base_config.renderers.append('chameleon_genshi') 
+#base_config.renderers.append('chameleon_genshi')
 
 #Configure the base SQLALchemy Setup
 base_config.use_sqlalchemy = True
@@ -31,7 +45,7 @@ base_config.sa_auth.group_class = model.Group
 # what is the class you want to use to search for permissions in the database
 base_config.sa_auth.permission_class = model.Permission
 
-# override this if you would like to provide a different who plugin for 
+# override this if you would like to provide a different who plugin for
 # managing login and logout of your application
 base_config.sa_auth.form_plugin = None
 
