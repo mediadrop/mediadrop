1aa41bfd7f06f05d5a43491dfff552ed4bc18ce1
Melanie Wright <mel@simplestation.com>
2009-03-13 23:53:38 -0700

    Filling in Recently Published table

--- mediaplex/controllers/admin.py
+++ mediaplex/controllers/admin.py
@@ -33,6 +33,9 @@ class AdminController(BaseController):
                             filter_by(reviewed=True, encoded=False).\
                             order_by(Video.date_added)[:6]
 
+        # TODO - update when date_published is available
+        recent = DBSession.query(Video).filter_by(reviewed=True, encoded=True).\
+            order_by(Video.date_modified)[:5]
         comments_to_review = DBSession.query(Comment).filter_by(reviewed=False).count()
         comments_total = DBSession.query(Comment).count()
 
@@ -40,4 +43,5 @@ class AdminController(BaseController):
                     media_to_encode=media_to_encode,
                     num_comments_to_review=comments_to_review,
                     num_comments_total=comments_total,
+                    recent=recent,
                     searchString=searchString)
