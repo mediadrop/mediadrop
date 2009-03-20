99e9e05de515dd4edb243b49d2fcb1e98a2574e2
Melanie Wright <mel@simplestation.com>
2009-03-20 15:59:57 -0700

    Updating dashboard for new model

--- mediaplex/controllers/admin.py
+++ mediaplex/controllers/admin.py
@@ -1,9 +1,10 @@
+from datetime import datetime
 from tg import expose, validate, flash, require, url, request, redirect
 from sqlalchemy import and_, or_
 from sqlalchemy.orm import eagerload
 
 from mediaplex.lib.base import BaseController
-from mediaplex.model import DBSession, Video, Comment, Tag
+from mediaplex.model import DBSession, Media, Video, Comment, Tag
 from mediaplex.controllers.video import VideoAdminController
 
 class AdminController(BaseController):
@@ -16,22 +17,27 @@ class AdminController(BaseController):
 
     @expose('mediaplex.templates.admin.index')
     def dashboard(self):
-        media_to_review = DBSession.query(Video).filter(Video.status == 'unreviewed').\
+        # Videos that are unreviewed are 'Awaiting Review'
+        video_to_review = DBSession.query(Video).filter(Video.status == 'unreviewed').\
             order_by(Video.created_on)[:6]
 
-        media_to_encode = DBSession.query(Video).options(eagerload('tags')).\
-            filter(Video.status == 'unreviewed').filter(Video.url == None).\
-            order_by(Video.created_on)[:6]
+        # Any publishable video that doesn't have a publish_on date is
+        # 'Awaiting Encoding', whether or not the url is None.
+        video_to_encode = DBSession.query(Video).options(eagerload('tags')).\
+            filter(Video.status == 'publishable').\
+            filter(Video.publish_on == None).order_by(Video.created_on)[:6]
 
-        # TODO - update when date_published is available
-        recent = DBSession.query(Video).filter(Video.status.in_(['publishable','draft'])).filter(Video.url != None).\
-            order_by(Video.modified_on)[:5]
+        # Any publishable video that does have a publish_on date that is in the
+        # past and is publishable is 'Recently Published'
+        recent = DBSession.query(Media).filter(Media.status == 'publishable').\
+            filter(Media.url != None).filter(Media.publish_on < datetime.now).\
+            order_by(Media.publish_on)[:5]
 
         comments_to_review = DBSession.query(Comment).filter_by(status='unreviewed').count()
         comments_total = DBSession.query(Comment).count()
 
-        return dict(media_to_review=media_to_review,
-                    media_to_encode=media_to_encode,
+        return dict(media_to_review=video_to_review,
+                    media_to_encode=video_to_encode,
                     num_comments_to_review=comments_to_review,
                     num_comments_total=comments_total,
                     recent=recent)
