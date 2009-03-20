d29b58cd20474d3826b2e9581ba22ff865c33afa
Melanie Wright <mel@simplestation.com>
2009-03-19 21:50:23 -0700

    Removing date published columns, search bar on dashboard

--- mediaplex/controllers/admin.py
+++ mediaplex/controllers/admin.py
@@ -11,23 +11,13 @@ class AdminController(BaseController):
     video = VideoAdminController()
 
     @expose()
-    def default(self, searchString='', *args):
-        redirect('/admin/dashboard/%s' % searchString)
+    def default(self, *args):
+        redirect('/admin/dashboard')
 
     @expose('mediaplex.templates.admin.index')
-    def dashboard(self, searchString=None):
-        media_to_review = DBSession.query(Video).filter(Video.status == 'unreviewed')
-
-        if searchString is not None:
-            like_search = '%%%s%%' % (searchString,)
-            media_to_review = media_to_review.outerjoin(Video.tags).\
-                filter(or_(Video.title.like(like_search),
-                           Video.description.like(like_search),
-                           Video.author_name.like(like_search),
-                           Video.notes.like(like_search),
-                           Video.tags.any(Tag.name.like(like_search))))
-
-        media_to_review = media_to_review.order_by(Video.created_on)[:6]
+    def dashboard(self):
+        media_to_review = DBSession.query(Video).filter(Video.status == 'unreviewed').\
+            order_by(Video.created_on)[:6]
 
         media_to_encode = DBSession.query(Video).options(eagerload('tags')).\
             filter(Video.status == 'unreviewed').filter(Video.url == None).\
@@ -44,5 +34,4 @@ class AdminController(BaseController):
                     media_to_encode=media_to_encode,
                     num_comments_to_review=comments_to_review,
                     num_comments_total=comments_total,
-                    recent=recent,
-                    searchString=searchString)
+                    recent=recent)
