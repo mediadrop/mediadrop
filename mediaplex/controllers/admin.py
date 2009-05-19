7f61e5e86cd1c3484319769e2f819ddf4ce157b7
Melanie Wright <mel@simplestation.com>
2009-05-19 16:03:58 -0700

    Adding 'Awaiting Publishing' box to dashboard

--- mediaplex/controllers/admin.py
+++ mediaplex/controllers/admin.py
@@ -5,46 +5,42 @@ from sqlalchemy.orm import eagerload
 from webhelpers import paginate
 
 from mediaplex.lib.base import BaseController
+from mediaplex.lib.helpers import expose_xhr
 from mediaplex.model import DBSession, Media, Video, Comment, Tag
 
 class AdminController(BaseController):
-    @expose('mediaplex.templates.admin.index')
-    def index(self):
-        # Any publishable video that does have a publish_on date that is in the
-        # past and is publishable is 'Recently Published'
-        recent_media = DBSession.query(Media).\
-            filter(Media.status >= 'publish').\
-            filter(Media.publish_on < datetime.now).\
-            order_by(Media.publish_on)[:5]
-        comments_pending_review = DBSession.query(Comment).filter(Comment.status >= 'pending_review').count()
-        comments_total = DBSession.query(Comment).count()
+    """Admin dashboard actions"""
 
-        return dict(review_page=self._fetch_review_page(),
-                    encode_page=self._fetch_encode_page(),
-                    num_comments_to_review=comments_pending_review,
-                    num_comments_total=comments_total,
-                    recent_media=recent_media)
+    @expose_xhr('mediaplex.templates.admin.index','mediaplex.templates.admin.video.dash-table')
+    def index(self, **kwargs):
+        if request.is_xhr:
+            """ShowMore Ajax Fetch Action"""
+            status = kwargs.get('status', 'pending_review')
+            page_num = kwargs.get('page_num', 1)
+            return dict(collection=self._fetch_page(status, page_num).items)
+        else:
+            # Any publishable video that does have a publish_on date that is in the
+            # past and is publishable is 'Recently Published'
+            recent_media = DBSession.query(Media).\
+                filter(Media.status.intersects('publish')).\
+                filter(Media.publish_on < datetime.now).\
+                order_by(Media.publish_on)[:5]
+            comments_pending_review = DBSession.query(Comment).filter(Comment.status >= 'pending_review').count()
+            comments_total = DBSession.query(Comment).count()
 
-    @expose('mediaplex.templates.admin.video.dash-table')
-    def ajax_review(self, page_num):
-        """ShowMore Ajax Fetch Action for Awaiting Review"""
-        return dict(collection=self._fetch_review_page(page_num).items, is_ajax=True)
+            return dict(review_page=self._fetch_page('pending_review'),
+                        encode_page=self._fetch_page('pending_encoding'),
+                        publish_page=self._fetch_page('draft'),
+                        num_comments_to_review=comments_pending_review,
+                        num_comments_total=comments_total,
+                        recent_media=recent_media)
 
-    def _fetch_review_page(self, page_num=1, items_per_page=6):
-        """Helper method for paginating video results"""
-        query = DBSession.query(Video).filter(Video.status >= 'pending_review').\
+    def _fetch_page(self, status='pending_review', page_num=1, items_per_page=6):
+        """Helper method for paginating media results"""
+        query = DBSession.query(Media).\
+            filter(Video.status.intersects(status)).\
+            filter(Video.status.excludes('trash')).\
             order_by(Video.created_on)
-        return paginate.Page(query, page_num, items_per_page)
 
-    @expose('mediaplex.templates.admin.video.dash-table')
-    def ajax_encode(self, page_num):
-        """ShowMore Ajax Fetch Action for Awaiting Encoding"""
-        return dict(collection=self._fetch_encode_page(page_num).items, is_ajax=True)
-
-    def _fetch_encode_page(self, page_num=1, items_per_page=6):
-        """Helper method for paginating video results"""
-        query = DBSession.query(Video).options(eagerload('tags')).\
-            filter(Video.status >= 'pending_encoding').\
-            order_by(Video.created_on)
         return paginate.Page(query, page_num, items_per_page)
 
