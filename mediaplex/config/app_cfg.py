85b2947ba5526083df43db34ab980201496e527b
Nathan Wright <nathan@simplestation.com>
2009-05-28 16:24:19 -0700

    Rework routes so that URL generation works

--- mediaplex/config/app_cfg.py
+++ mediaplex/config/app_cfg.py
@@ -15,8 +15,8 @@ class MediaplexConfig(AppConfig):
         map.redirect('/', '/video-flow')
 
         # routes for all non-view, non-index, video actions
-        map.connect('/video-{action}/{slug}', slug=None, controller='video', requirements=dict(action='tags|flow|flow_ajax|upload|upload_submit|upload_submit_async|upload_success'))
-        map.connect('/video-rate/{slug}/{rating}', controller='video', action='rate')
+        map.connect('/video-{action}', controller='video', requirements=dict(action='flow|upload|upload_submit|upload_submit_async|upload_success'))
+        map.connect('/video-{action}/{slug}', slug=None, controller='video', requirements=dict(action='tags|rate'))
         # route for viewing videos and other video related actions
         map.connect('/video/{slug}/{action}', controller='video', action='view')
         # admin routes
