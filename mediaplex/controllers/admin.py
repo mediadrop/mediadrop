951d553c3875cccd00e21421c37c7b70456fec4d
Melanie Wright <mel@simplestation.com>
2009-05-28 17:23:05 -0700

    Filtering status on dashboard properly

--- mediaplex/controllers/admin.py
+++ mediaplex/controllers/admin.py
@@ -17,7 +17,7 @@ class AdminController(RoutingController):
     def index(self, **kwargs):
         if request.is_xhr:
             """ShowMore Ajax Fetch Action"""
-            status = kwargs.get('status', 'pending_review')
+            type = kwargs.get('type', 'awaiting_review')
             page = kwargs.get('page', 1)
             return dict(collection=self._fetch_page(status, page).items)
         else:
@@ -31,18 +31,26 @@ class AdminController(RoutingController):
             comments_total = DBSession.query(Comment).count()
 
             return dict(
-                review_page=self._fetch_page('pending_review'),
-                encode_page=self._fetch_page('pending_encoding'),
-                publish_page=self._fetch_page('draft'),
+                review_page=self._fetch_page('awaiting_review'),
+                encode_page=self._fetch_page('awaiting_encoding'),
+                publish_page=self._fetch_page('awaiting_publishing'),
                 num_comments_to_review=comments_pending_review,
                 num_comments_total=comments_total,
                 recent_media=recent_media
             )
 
-    def _fetch_page(self, status='pending_review', page=1, items_per_page=6):
+    def _fetch_page(self, type='awaiting_review', page=1, items_per_page=6):
         """Helper method for paginating media results"""
-        query = DBSession.query(Media).\
-            filter(Video.status.intersects(status)).\
-            filter(Video.status.excludes('trash')).\
-            order_by(Video.created_on)
+
+        query = DBSession.query(Media).order_by(Video.created_on)
+
+        if type == 'awaiting_review':
+            query = query.filter(Video.status.intersects('pending_review')).\
+                         filter(Video.status.excludes('trash'))
+        elif type == 'awaiting_encoding':
+            query = query.filter(Video.status.intersects('pending_encoding')).\
+                         filter(Video.status.excludes('trash,pending_review'))
+        elif type == 'awaiting_publishing':
+            query = query.filter(Video.status.issubset('draft'))
+
         return webhelpers.paginate.Page(query, page, items_per_page)
