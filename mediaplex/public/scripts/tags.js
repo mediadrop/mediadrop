1eb93ddf658cfdc6b3a23f5479dc94a909336328
Nathan Wright <nathan@simplestation.com>
2009-06-04 20:04:33 -0700

    Add topic index with slider

--- mediaplex/public/scripts/tags.js
+++ mediaplex/public/scripts/tags.js
@@ -1,31 +1,98 @@
-var TagSlider = new Class({
-	Extends: Options,
+/**
+ * Slider that operates on the elements of a container one after another.
+ *
+ * Each element can have different Fx.Slide options which can also change
+ * depending on whether the slider is opening (sliding 'in' to view) or
+ * closing (sliding 'out' of view).
+ *
+ * @author Nathan Wright
+ */
+var SeriesSlider = new Class({
+
+	Implements: [Options, Chain],
+
 	options: {
-		container: 'mediaplex-tags',
-		fxOptions: {transition: 'quad:in:out'},
-		button: 'mediaplex-ctrls-tags',
-		slideWrapperStyles: {clear: 'both'},
-		startHidden: true
+		elements: '',
+		button: '',
+		buttonActiveClass: '',
+		startHidden: true,
+		fx: {}, // defaults overridden by openFx & closeFx
+		openFx: [], // on open fx options per element
+		closeFx: [] // on open fx options per element
 	},
 
-	container: null,
+	elements: null,
 	button: null,
+	open: true,
+	fx: [],
 
 	initialize: function(opts){
 		this.setOptions(opts);
-		this.container = $(this.options.container).set('slide', this.fxOptions);
-		if (this.options.startHidden) {
-			this.container.slide('hide');
+		this.elements = $$(this.options.elements);
+		for (var i = this.elements.length; i--; i) {
+			this.fx[i] = this.elements[i].get('slide', this.options.closeFx[i] || this.options.fx)
+				.addEvent('complete', this.callChain.bind(this));
 		}
-		if (this.options.slideWrapperStyles) {
-			this.container.get('slide').wrapper.setStyles(this.options.slideWrapperStyles);
-		}
-		this.button = $(this.options.button).addEvent('click', this.slideToggle.bind(this));
+		this.button = $(this.options.button).addEvent('click', this.toggle.bind(this));
+		if (this.options.startHidden) this.hide();
 	},
 
-	slideToggle: function(){
-		this.button.toggleClass('active');
-		this.container.slide('toggle');
-		return false;
+	/** Slide 'in' or 'out' of view */
+	slide: function(how){
+		this.clearChain();
+		if (how == 'in') {
+			for (var i = 0; i < this.fx.length; i++) this.chain(this._startFx.bind(this, [i, how]));
+			this._setOpen(true);
+		} else {
+			for (var i = this.fx.length; i--; i) this.chain(this._startFx.bind(this, [i, how]));
+			this.chain(this._setOpen.bind(this, [false, true]));
+		}
+		this.chain(this._swapFxOptions.bind(this));
+		return this.callChain();
+	},
+
+	toggle: function(e){
+		if (e != undefined) new Event(e).preventDefault();
+		if (this.open) return this.slide('out');
+		else return this.slide('in');
+	},
+
+	show: function(e){
+		if (e != undefined) new Event(e).preventDefault();
+		return this._showHide('show');
+	},
+
+	hide: function(e){
+		if (e != undefined) new Event(e).preventDefault();
+		return this._showHide('hide');
+	},
+
+	_showHide: function(how){
+		this.clearChain();
+		for (var i = this.fx.length; i--; i) this.fx[i][how]();
+		return this._setOpen(how == 'show')
+		           ._swapFxOptions();
+	},
+
+	_setOpen: function(flag, callChainAfter){
+		this.open = !!flag;
+		if (this.open) {
+			this.button.addClass(this.options.buttonActiveClass);
+		} else {
+			this.button.removeClass(this.options.buttonActiveClass);
+		}
+		if (callChainAfter) this.callChain();
+		return this;
+	},
+
+	_startFx: function(i, mode){
+		var thisFx = this.fx[i];
+		thisFx.start.run(mode, thisFx);
+	},
+
+	_swapFxOptions: function(){
+		var opts = this.open ? this.options.closeFx : this.options.openFx;
+		for (var i = this.fx.length; i--; i) this.fx[i].setOptions(opts[i] || this.options.fx);
+		return this;
 	}
 });
