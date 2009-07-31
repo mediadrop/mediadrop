0ef68c2878001f50304d65ba02cd60c6d1272880
Anthony Theocharis <anthony@simplestation.com>
2009-07-30 19:35:46 -0700

    Update error controller to be routable and implement template.

--- mediaplex/controllers/error.py
+++ mediaplex/controllers/error.py
@@ -2,12 +2,12 @@ import os.path
 
 import paste.fileapp
 import tg
-from tg import expose, TGController
+from tg import expose
 from pylons.controllers.util import forward
 from pylons.middleware import error_document_template, media_path
-from mediaplex.lib.base import Controller, RoutingController
+from mediaplex.lib.base import RoutingController
 
-class ErrorController(TGController):
+class ErrorController(RoutingController):
     """Generates error documents as and when they are required.
 
     The ErrorDocuments middleware forwards to ErrorController when error
@@ -19,11 +19,27 @@ class ErrorController(TGController):
 
     @expose('mediaplex.templates.error')
     def document(self, *args, **kwargs):
-        """Render the error document"""
+        """Render the error document
+
+        This method is called in a secondary dispatch initiated in
+        pylons.middleware.StatusCodeRedirect.__call__()
+        This call sets some extra environment variables that you
+        might be interested in.
+
+        The dispatch essentially initiates a second request, for the URL
+        /error/document
+
+        The URL /error/document is set on initialization of the
+        StatusCodeRedirect object, and can be overridden in
+        tg.configuration.add_error_middleware()
+        """
+
         resp = tg.request.environ.get('pylons.original_response')
         default_message = ("<p>We're sorry but we weren't able to process "
         " this request.</p>")
-        values = dict(prefix=tg.request.environ.get('SCRIPT_NAME', ''),
-                 code=tg.request.params.get('code', resp.status_int),
-                 message=tg.request.params.get('message', default_message))
-        return values
+        return dict(
+            prefix = tg.request.environ.get('SCRIPT_NAME', ''),
+            code = tg.request.params.get('code', resp.status_int),
+            message = tg.request.params.get('message', default_message)
+        )
+
