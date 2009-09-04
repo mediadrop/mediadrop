cc52992f8b48056e150af7bcaa2a7ef0307e05f9
Nathan Wright <nathan@simplestation.com>
2009-09-03 18:01:52 -0700

    Port the comments box from admin/media to the admin dash

--- mediaplex/controllers/admin.py
+++ mediaplex/controllers/admin.py
@@ -22,39 +22,50 @@ class AdminController(RoutingController):
             .filter(Media.status.excludes('trash'))\
             .filter(Media.publish_on < datetime.now)\
             .order_by(Media.publish_on.desc())[:5]
-        comments_unreviewed = DBSession.query(Comment)\
+
+        comment_count = DBSession.query(Comment).count()
+        comment_count_published = DBSession.query(Comment)\
+            .filter(Comment.status >= 'publish')\
+            .filter(Comment.status.excludes('trash'))\
+            .count()
+        comment_count_unreviewed = DBSession.query(Comment)\
             .filter(Comment.status >= 'unreviewed')\
             .filter(Comment.status.excludes('trash'))\
             .count()
-        comments_total = DBSession.query(Comment).count()
+        comment_count_trash = DBSession.query(Comment)\
+            .filter(Comment.status >= 'trash')\
+            .count()
 
         return dict(
-            review_page=self._fetch_page('awaiting_review'),
-            encode_page=self._fetch_page('awaiting_encoding'),
-            publish_page=self._fetch_page('awaiting_publishing'),
-            num_comments_to_review=comments_unreviewed,
-            num_comments_total=comments_total,
-            recent_media=recent_media
+            review_page = self._fetch_page('awaiting_review'),
+            encode_page = self._fetch_page('awaiting_encoding'),
+            publish_page = self._fetch_page('awaiting_publishing'),
+            recent_media = recent_media,
+            comment_count = comment_count,
+            comment_count_published = comment_count_published,
+            comment_count_unreviewed = comment_count_unreviewed,
+            comment_count_trash = comment_count_trash,
         )
+
 
     @expose('mediaplex.templates.admin.media.dash-table')
     def video_table(self, table, page, **kwargs):
         """ShowMore Ajax Fetch Action"""
         return dict(
-            media=self._fetch_page(table, page).items,
+            media = self._fetch_page(table, page).items,
         )
+
 
     def _fetch_page(self, type='awaiting_review', page=1, items_per_page=6):
         """Helper method for paginating media results"""
-
         query = DBSession.query(Media).order_by(Media.modified_on.desc())
 
         if type == 'awaiting_review':
-            query = query.filter(Media.status.intersects('unreviewed')).\
-                         filter(Media.status.excludes('trash'))
+            query = query.filter(Media.status.intersects('unreviewed'))\
+                         .filter(Media.status.excludes('trash'))
         elif type == 'awaiting_encoding':
-            query = query.filter(Media.status.intersects('unencoded')).\
-                         filter(Media.status.excludes('trash,unreviewed'))
+            query = query.filter(Media.status.intersects('unencoded'))\
+                         .filter(Media.status.excludes('trash,unreviewed'))
         elif type == 'awaiting_publishing':
             query = query.filter(Media.status.issubset('draft'))
 
