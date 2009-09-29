2badd8244ef748366d9db7a21d61403d30fd2eae
Nathan Wright <nathan@simplestation.com>
2009-09-29 16:43:06 -0700

    Cleanup settingscontroller and remove links to the *unfinished* user panel

--- simpleplex/controllers/settingadmin.py
+++ simpleplex/controllers/settingadmin.py
@@ -1,6 +1,6 @@
 from string import capitalize, rstrip
 from tg import expose, validate, flash, require, url, request
-from tg.decorators import paginate
+from tg.exceptions import HTTPNotFound
 from formencode import validators
 from pylons.i18n import ugettext as _
 from sqlalchemy import and_, or_
@@ -19,9 +19,9 @@ class SettingadminController(RoutingController):
     allow_only = has_permission('admin')
 
     @expose()
-    def index(self, section='users', **kwargs):
-        if section == 'topics' or section == 'tags':
+    def index(self, section='tags', **kwargs):
+        if section in ('topics', 'tags'):
             redirect(controller='categoryadmin', category=section)
         if section == 'users':
             redirect(controller='useradmin')
-        redirect('404.html')
+        raise HTTPNotFound
