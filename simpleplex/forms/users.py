7bdc6d10fef8bb68e179c50774775e70cccf00a5
Melanie Wright <mel@simplestation.com>
2009-10-11 19:20:26 -0700

    Using fieldset for users login details
    Figured out how to use FieldMatch with ListFieldSets

--- simpleplex/forms/users.py
+++ simpleplex/forms/users.py
@@ -1,5 +1,4 @@
-from tw.forms.validators import NotEmpty, Email, Schema, FancyValidator, All, PlainText, FieldsMatch
-
+from tw.forms.validators import Invalid, NotEmpty, Email, Schema, FancyValidator, All, PlainText, FieldsMatch
 from tg import request
 
 from simpleplex.forms import Form, ListForm, ListFieldSet, TextField, XHTMLTextArea, FileField, CalendarDatePicker, SingleSelectField, TextArea, SubmitButton, Button, HiddenField, CheckBoxList, PasswordField
@@ -16,7 +15,7 @@ class UniqueUsername(FancyValidator):
             query = query.filter(User.user_id != id)
 
         if query.count() != 0:
-            raise formencode.Invalid(
+            raise Invalid(
                 'User name already exists',
                 value, state)
         return value
@@ -27,25 +26,22 @@ class UserForm(ListForm):
     css_class = 'form'
     submit_text = None
     show_children_errors = True
-    validator = Schema(
-        allow_extra_fields=True, # Allow extra kwargs that tg likes to pass: pylons, start_request, environ...
-        chained_validators=(FieldsMatch('password',
-                                        'confirm_password',
-                                        not_empty=True,
-                                        messages={'invalidNoMatch': "Passwords do not match",}),),
-    )
     _name = 'user-form' # TODO: Figure out why this is required??
 
     fields = [
         TextField('display_name', validator=NotEmpty, maxlength=255),
-        TextField('email_address', validator=Email(not_empty=True, messages={
-            'badUsername': 'The portion of the email address before the @ is invalid',
-            'badDomain': 'The portion of this email address after the @ is invalid'
-        }), maxlength=255),
-        SingleSelectField('group', label_text='Group', options=lambda: DBSession.query(Group.group_id, Group.display_name).all()),
-        TextField('user_name', maxlength=16, validator=All(PlainText(), UniqueUsername(not_empty=True))),
-        PasswordField('password', validators=NotEmpty, maxlength=80, autocomplete='off'),
-        PasswordField('confirm_password', validators=NotEmpty, maxlength=80),
+        TextField('email_address', validator=NotEmpty, maxlength=255),
+        ListFieldSet('login_details', suppress_label=True, legend='Login Details:',
+                     css_classes=['details_fieldset'],
+                     validator = Schema(chained_validators=[FieldsMatch('password',
+                                                                        'confirm_password',
+                                                                        messages={'invalidNoMatch': "Passwords do not match",})]),
+                     children=[
+            SingleSelectField('group', label_text='Group', options=lambda: DBSession.query(Group.group_id, Group.display_name).all()),
+            TextField('user_name', maxlength=16, validator=All(PlainText(), UniqueUsername(not_empty=True))),
+            PasswordField('password', validators=NotEmpty, maxlength=80, autocomplete='off'),
+            PasswordField('confirm_password', validators=NotEmpty, maxlength=80),
+        ]),
         SubmitButton('save', default='Save', named_button=True, css_classes=['mo', 'btn-save', 'f-rgt']),
         SubmitButton('delete', default='Delete', named_button=True, css_classes=['mo', 'btn-delete']),
     ]
