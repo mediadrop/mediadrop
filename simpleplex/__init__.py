e93ad3a6418a8500bf5790ebd06fc3e18c47e69a
Nathan Wright <nathan@simplestation.com>
2009-10-13 13:58:27 -0700

    Add simpleplex.ipython method
    Acts as a breakpoint where an ipython shell is opened, you can tinker,
    and once you're done the request continues.
    
    Usage: simpleplex.ipython()()

--- simpleplex/__init__.py
+++ simpleplex/__init__.py
@@ -1,7 +1,23 @@
 import sys
 def debug(*args):
-       # print to stderr, for debuging
-       # TODO: Perhaps replace with with more robust logging.
-       print >> sys.stderr, "Simpleplex DEBUG", args
-       return None
+    """Print to stderr, for debuging"""
+    print >> sys.stderr, "Simpleplex DEBUG", args
+    return None
 
+def ipython():
+    """Launch an ipython shell where called before finishing the request.
+
+    This only works with the Pylons paster server, and obviously ipython
+    must be installed as well. Usage::
+
+    simpleplex.ipython()()
+
+    """
+    from IPython.Shell import IPShellEmbed
+    args = ['-pdb', '-pi1', 'In <\\#>: ', '-pi2', '   .\\D.: ',
+            '-po', 'Out<\\#>: ', '-nosep']
+    return IPShellEmbed(
+        args,
+        banner='Entering IPython.  Press Ctrl-D to exit.',
+        exit_msg='Leaving Interpreter, back to Pylons.'
+    )
