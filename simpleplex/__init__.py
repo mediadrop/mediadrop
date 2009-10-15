71cf721ccef8b803d45e4bf804a0f869228ff5b2
Anthony Theocharis <anthony@simplestation.com>
2009-10-15 12:32:35 -0700

    Monkey Patch BeautifulSoup, until the library itself is updated.
    relevant bug: http://bugs.launchpad.net/beautifulsoup/+bug/397997

--- simpleplex/__init__.py
+++ simpleplex/__init__.py
@@ -1,7 +1,25 @@
-import sys
-def debug(*args):
-       # print to stderr, for debuging
-       # TODO: Perhaps replace with with more robust logging.
-       print >> sys.stderr, "Simpleplex DEBUG", args
-       return None
+def monkeypatch_method(cls):
+    """MonkeyPatching decorator
 
+    Described by Guido van Rossum, here:
+        http://mail.python.org/pipermail/python-dev/2008-January/076194.html
+    """
+    def decorator(func):
+        setattr(cls, func.__name__, func)
+        return func
+    return decorator
+
+# Monkey Patch Beautiful Soup
+from BeautifulSoup import NavigableString
+@monkeypatch_method(NavigableString)
+def __eq__(self, other):
+    """Monkey-patch inserted method.
+
+    This patch is a temporary solution to the problem described here:
+        http://bugs.launchpad.net/beautifulsoup/+bug/397997
+    """
+    if isinstance(other, NavigableString):
+        return other is self
+    else:
+        return unicode.__eq__(self, other)
+del NavigableString
