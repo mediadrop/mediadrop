3b6a3e0370c3c5f4b73adaab8e1e1904bb832681
Nathan Wright <nathan@simplestation.com>
2009-12-07 04:50:49 -0800

    Fix url generation for to '/' (MediaController.index)
    When the default action isn't in the allowed action list, that route
    will never be generated. Fixes an issue where the url /media is
    generated instead of the correct / -- ending up in a confused redirect
    to a 404.

--- mediacore/config/routing.py
+++ mediacore/config/routing.py
@@ -31,7 +31,7 @@ def make_map():
         controller='media',
         action='index',
         requirements={'action': ('flow|upload|upload_submit|upload_success|'
-                                 'upload_submit_async|upload_failure')})
+                                 'upload_submit_async|upload_failure|index')})
 
     # Podcast list actions
     map.connect('/podcasts/feed/{slug}.xml',
