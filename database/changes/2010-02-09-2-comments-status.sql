88325e5b66059093bc29d984c391a3be6dabd2c5
Nathan Wright <nathan@simplestation.com>
2010-02-23 13:21:53 -0800

    Fix comment status DB change script so comments stay trashed
    If you've already run the broken version of this script, you may correct
    the problem if you still have an old copy of your data! Temporarily
    restore the comments table under a different name and copy the data
    over. Assuming you call the table comments_old, run this query:
    
    UPDATE comments SET reviewed = 1, publishable = 0
    WHERE id IN (SELECT comments_old.id FROM comments_old
    	WHERE comments_old.status LIKE '%trash%');
    
    After that you can drop the comments_old table again.

--- database/changes/2010-02-09-2-comments-status.sql
+++ database/changes/2010-02-09-2-comments-status.sql
@@ -2,8 +2,8 @@ ALTER TABLE `comments`
 	ADD COLUMN `reviewed` TINYINT(1) UNSIGNED NOT NULL DEFAULT 0 AFTER `status`,
 	ADD COLUMN `publishable` TINYINT(1) UNSIGNED NOT NULL DEFAULT 0 AFTER `reviewed`;
 
-UPDATE comments SET reviewed = 0 WHERE status LIKE '%unreviewed%';
-UPDATE comments SET reviewed = 1 WHERE status NOT LIKE '%unreviewed%';
-UPDATE comments SET publishable = 1 WHERE status LIKE '%publish%';
+UPDATE comments SET reviewed = 0, publishable = 0 WHERE status LIKE '%unreviewed%';
+UPDATE comments SET reviewed = 1, publishable = 1 WHERE status LIKE '%publish%';
+UPDATE comments SET reviewed = 1, publishable = 0 WHERE status LIKE '%trash%';
 
 ALTER TABLE comments DROP COLUMN `status`;
