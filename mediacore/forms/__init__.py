45fde093d9ee49bf1bf88027eb4b557491a29bc7
Anthony Theocharis <anthony@simplestation.com>
2010-02-26 19:20:49 -0800

    Disable url rewriting inside tinymce.
    Tinymce was attempting to clean up urls in links, but was doing so relative
    to the edit page. This broke the urls when accessed from the view pages.

--- mediacore/forms/__init__.py
+++ mediacore/forms/__init__.py
@@ -90,7 +90,9 @@ tinyMCE.init({
 	theme_advanced_toolbar_location: "top",
 	theme_advanced_toolbar_align: "left",
 	theme_advanced_statusbar_location: "bottom",
-	theme_advanced_resizing: false
+	theme_advanced_resizing: false,
+	relative_urls : false,
+	remove_script_host : false
 });
 });""", location='headbottom')
     ]
