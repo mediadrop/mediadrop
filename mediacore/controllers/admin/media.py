b4c7e61529ffc66e71db95d8e6630e63fa33a4fe
Nathan Wright <nathan@simplestation.com>
2010-04-08 15:58:14 -0700

    Fix admin media and podcast save actions, broken by a bad S/R

--- mediacore/controllers/admin/media.py
+++ mediacore/controllers/admin/media.py
@@ -222,7 +222,7 @@ class MediaController(BaseController):
         media.slug = get_available_slug(Media, slug, media)
         media.title = title
         media.author = Author(author_name, author_email)
-        media.description = helpers.clean_xhtml(description)
+        media.description = helpers.clean_admin_xhtml(description)
         media.notes = notes
         media.duration = details['duration'] # validator converts hh:mm:ss to secs
         media.podcast_id = podcast
