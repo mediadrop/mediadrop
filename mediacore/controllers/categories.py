7240af702178cca0ef3b5a3af78b6683b8545b63
Nathan Wright <nathan@simplestation.com>
2010-04-07 17:38:11 -0700

    Simplify how the grid is spaced

--- mediacore/controllers/categories.py
+++ mediacore/controllers/categories.py
@@ -54,7 +54,7 @@ class CategoriesController(BaseController):
 
         latest = media.order_by(Media.publish_on.desc())[:5]
         popular = media.order_by(Media.popularity_points.desc())\
-            .filter(sql.not_(Media.id.in_([m.id for m in latest])))[:5]
+            .filter(sql.not_(Media.id.in_([m.id for m in latest])))[:6]
 
         return dict(
             latest = latest,
