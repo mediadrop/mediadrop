mediadrop/mediadrop

commit bdaabfc8c3b74d4194ad0d9911b2209dbee25700

Move admin controllers to their own subdirectory.

mediacore/controllers/admin/media.py
@@ -48,10 +48,10 @@ add_file_form = AddFileForm()
 edit_file_form = EditFileForm()
 thumb_form = ThumbForm()
 update_status_form = UpdateStatusForm()
-search_form = SearchForm(action=url_for(controller='/mediaadmin', action='index'))
-podcast_filter_form = PodcastFilterForm(action=url_for(controller='/mediaadmin', action='index'))
+search_form = SearchForm(action=url_for(controller='/admin/media', action='index'))
+podcast_filter_form = PodcastFilterForm(action=url_for(controller='/admin/media', action='index'))
 
-class MediaadminController(BaseController):
+class MediaController(BaseController):
     allow_only = has_permission('admin')
 
     @expose_xhr('mediacore.templates.admin.media.index',
@@ -222,7 +222,7 @@ class MediaadminController(BaseController):
         media.slug = get_available_slug(Media, slug, media)
         media.title = title
         media.author = Author(author_name, author_email)
-        media.description = helpers.clean_admin_xhtml(description)
+        media.description = helpers.admin/clean__xhtml(description)
         media.notes = notes
         media.duration = details['duration'] # validator converts hh:mm:ss to secs
         media.podcast_id = podcast

mediacore/config/routing.py
@@ -73,59 +73,59 @@ def make_map():
     ################
 
     map.connect('/admin/media_table/{table}/{page}',
-        controller='admin',
+        controller='admin/admin',
         action='media_table')
 
     map.connect('/admin/media',
-        controller='mediaadmin',
+        controller='admin/media',
         action='index')
     map.connect('/admin/media/{id}/{action}',
-        controller='mediaadmin',
+        controller='admin/media',
         action='edit')
 
     map.connect('/admin/podcasts',
-        controller='podcastadmin',
+        controller='admin/podcast',
         action='index')
     map.connect('/admin/podcasts/{id}/{action}',
-        controller='podcastadmin',
+        controller='admin/podcast',
         action='edit')
 
     map.connect('/admin/comments',
-        controller='commentadmin',
+        controller='admin/comment',
         action='index')
     map.connect('/admin/comments/{id}/{status}',
-        controller='commentadmin',
+        controller='admin/comment',
         action='save_status',
         requirements={'status': 'approve|trash'})
 
     map.connect('/admin/settings',
-        controller='categoryadmin',
+        controller='admin/category',
         action='index')
 
     map.connect('/admin/settings/notifications/{action}',
-        controller='notificationadmin')
+        controller='admin/notification')
 
     map.connect('/admin/settings/display/{action}',
-        controller='displayadmin')
+        controller='admin/display')
 
     map.connect('/admin/settings/users',
-        controller='useradmin',
+        controller='admin/user',
         action='index')
     map.connect('/admin/settings/users/{id}/{action}',
-        controller='useradmin',
+        controller='admin/user',
         action='edit')
 
     map.connect('/admin/settings/tags',
-        controller='tagadmin',
+        controller='admin/tag',
         action='index')
     map.connect('/admin/settings/tags/{id}/{action}',
-        controller='tagadmin',
+        controller='admin/tag',
         action='edit')
     map.connect('/admin/settings/categories',
-        controller='categoryadmin',
+        controller='admin/category',
         action='index')
     map.connect('/admin/settings/categories/{id}/{action}',
-        controller='categoryadmin',
+        controller='admin/category',
         action='edit')
 
     ##############

mediacore/controllers/admin/category.py
@@ -28,7 +28,7 @@ from mediacore.forms.admin.settings.categories import CategoryForm, CategoryRowF
 category_form = CategoryForm()
 category_row_form = CategoryRowForm()
 
-class CategoryadminController(BaseController):
+class CategoryController(BaseController):
     allow_only = has_permission('admin')
 
     @expose('mediacore.templates.admin.settings.categories.index')

mediacore/controllers/admin/comment.py
@@ -29,10 +29,10 @@ from mediacore.forms.admin.comments import EditCommentForm
 
 
 edit_form = EditCommentForm()
-search_form = SearchForm(action=url_for(controller='/commentadmin',
+search_form = SearchForm(action=url_for(controller='/admin/comment',
                                         action='index'))
 
-class CommentadminController(BaseController):
+class CommentController(BaseController):
     allow_only = has_permission('admin')
 
     @expose_xhr('mediacore.templates.admin.comments.index',

mediacore/controllers/admin/display.py
@@ -24,10 +24,10 @@ from mediacore.model import DBSession, fetch_row, Setting
 from mediacore.forms.admin.settings.settings import SettingsForm, DisplaySettingsForm
 
 display_settings_form = DisplaySettingsForm(
-    action=url_for(controller='/displayadmin', action='save'))
+    action=url_for(controller='/admin/display', action='save'))
 
 
-class DisplayadminController(BaseController):
+class DisplayController(BaseController):
     allow_only = has_permission('admin')
 
     @expose('mediacore.templates.admin.settings.display.edit')

mediacore/controllers/admin/notification.py
@@ -24,10 +24,10 @@ from mediacore.model import DBSession, fetch_row, Setting
 from mediacore.forms.admin.settings.settings import SettingsForm, DisplaySettingsForm
 
 settings_form = SettingsForm(
-    action=url_for(controller='/notificationadmin', action='save'))
+    action=url_for(controller='/admin/notification', action='save'))
 
 
-class NotificationadminController(BaseController):
+class NotificationController(BaseController):
     allow_only = has_permission('admin')
 
     @expose('mediacore.templates.admin.settings.notifications.edit')

mediacore/controllers/admin/podcast.py
@@ -36,7 +36,7 @@ from mediacore.forms.admin.podcasts import PodcastForm
 podcast_form = PodcastForm()
 thumb_form = ThumbForm()
 
-class PodcastadminController(BaseController):
+class PodcastController(BaseController):
     allow_only = has_permission('admin')
 
     @expose_xhr('mediacore.templates.admin.podcasts.index',
@@ -148,7 +148,7 @@ class PodcastadminController(BaseController):
         podcast.title = title
         podcast.subtitle = subtitle
         podcast.author = Author(author_name, author_email)
-        podcast.description = helpers.clean_admin_xhtml(description)
+        podcast.description = helpers.admin/clean__xhtml(description)
         podcast.copyright = details['copyright']
         podcast.category = details['category']
         podcast.itunes_url = details['itunes_url']

commit c802ccf5ac1d668a63463583619d04511fac49fb

Fix two  typos introduced in bdaabfc8c3b74d4194ad0d9911b2209dbee25700

mediacore/controllers/admin/media.py
@@ -222,7 +222,7 @@ class MediaController(BaseController):
         media.slug = get_available_slug(Media, slug, media)
         media.title = title
         media.author = Author(author_name, author_email)
-        media.description = helpers.admin/clean__xhtml(description)
+        media.description = helpers.clean_xhtml(description)
         media.notes = notes
         media.duration = details['duration'] # validator converts hh:mm:ss to secs
         media.podcast_id = podcast

mediacore/controllers/admin/podcasts.py
@@ -148,7 +148,7 @@ class PodcastsController(BaseController):
         podcast.title = title
         podcast.subtitle = subtitle
         podcast.author = Author(author_name, author_email)
-        podcast.description = helpers.admin/clean__xhtml(description)
+        podcast.description = helpers.clean_xhtml(description)
         podcast.copyright = details['copyright']
         podcast.category = details['category']
         podcast.itunes_url = details['itunes_url']