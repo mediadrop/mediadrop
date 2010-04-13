d47c22b5a0defcd2613b53fa054d1385d1f3227a
Anthony Theocharis <anthony@simplestation.com>
2010-04-13 14:07:10 -0700

    Fix post comment form to accept xhtml input like it's supposed to.

--- mediacore/forms/comments.py
+++ mediacore/forms/comments.py
@@ -16,7 +16,7 @@
 from tw.forms.validators import NotEmpty
 from tw.api import WidgetsList
 
-from mediacore.forms import ListForm, SubmitButton, TextField, TextArea, email_validator
+from mediacore.forms import ListForm, SubmitButton, TextField, TextArea, XHTMLValidator, email_validator
 
 
 class PostCommentForm(ListForm):
@@ -27,7 +27,7 @@ class PostCommentForm(ListForm):
     class fields(WidgetsList):
         name = TextField(validator=TextField.validator(not_empty=True, messages={'empty': 'Please enter your name!'}), maxlength=50)
         email = TextField(validator=email_validator(), label_text='Email Address (will never be published)', show_error=True, maxlength=255)
-        body = TextArea(label_text='Comment', attrs=dict(rows=5, cols=25))
+        body = TextArea(validator=XHTMLValidator, label_text='Comment', attrs=dict(rows=5, cols=25))
         submit = SubmitButton(default='Post Comment', css_class='btn btn-post-comment')
 
 
