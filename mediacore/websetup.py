5e4dfb98b5f9220f8f5956838ab9822a7d96f7d6
Anthony Theocharis <anthony@simplestation.com>
2010-04-13 17:06:27 -0700

    Update websetup.py

--- mediacore/websetup.py
+++ mediacore/websetup.py
@@ -17,37 +17,9 @@ def setup_app(command, conf, vars):
     print "Creating tables"
     model.metadata.create_all(bind=config['pylons.app_globals'].sa_engine)
 
-    u = model.User()
-    u.user_name = u'manager'
-    u.display_name = u'Example manager'
-    u.email_address = u'manager@somedomain.com'
-    u.password = u'managepass'
+    # Set up any useful initial data here.
+    # TODO: Use this file to replace setup.sql
 
-    model.DBSession.add(u)
-
-    g = model.Group()
-    g.group_name = u'managers'
-    g.display_name = u'Managers Group'
-
-    g.users.append(u)
-
-    model.DBSession.add(g)
-
-    p = model.Permission()
-    p.permission_name = u'manage'
-    p.description = u'This permission give an administrative right to the bearer'
-    p.groups.append(g)
-
-    model.DBSession.add(p)
-
-    u1 = model.User()
-    u1.user_name = u'editor'
-    u1.display_name = u'Example editor'
-    u1.email_address = u'editor@somedomain.com'
-    u1.password = u'editpass'
-
-    model.DBSession.add(u1)
     model.DBSession.flush()
-
     transaction.commit()
     print "Successfully setup"
