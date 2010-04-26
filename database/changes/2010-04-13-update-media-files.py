908cb8c2133705f9bcba615749df1b6a5388663a
Anthony Theocharis <anthony@simplestation.com>
2010-04-26 16:44:34 -0700

    fix to media update database script

--- database/changes/2010-04-13-update-media-files.py
+++ database/changes/2010-04-13-update-media-files.py
@@ -1,8 +1,6 @@
 from urlparse import urlparse
 import os.path
 import transaction
-import pkg_resources
-pkg_resources.require('TurboGears2')
 
 from mediacore.model import *
 from sqlalchemy import create_engine, select
