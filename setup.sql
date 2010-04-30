d736a1d8a02e2911437b834ee903dc9ccc2d8b3f
Anthony Theocharis <anthony@simplestation.com>
2010-04-30 16:50:12 -0700

    Update setup.sql to contain the 0.8 database model.

--- setup.sql
+++ setup.sql
@@ -41,7 +41,7 @@ CREATE TABLE `categories` (
 
 LOCK TABLES `categories` WRITE;
 /*!40000 ALTER TABLE `categories` DISABLE KEYS */;
-INSERT INTO `categories` VALUES (1,'Featured','featured',NULL),
+INSERT INTO `categories` VALUES (1,'Example Topic','example-topic',NULL),
 (2,'Another Subject','another-subject',NULL);
 /*!40000 ALTER TABLE `categories` ENABLE KEYS */;
 UNLOCK TABLES;
@@ -118,7 +118,7 @@ CREATE TABLE `comments` (
   `author_ip` int(10) unsigned NOT NULL,
   `body` text NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `comments_media` (`media_id`),
+  KEY `comments_media_fk1` (`media_id`),
   CONSTRAINT `comments_media_fk1` FOREIGN KEY (`media_id`) REFERENCES `media` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -131,6 +131,60 @@ LOCK TABLES `comments` WRITE;
 /*!40000 ALTER TABLE `comments` DISABLE KEYS */;
 INSERT INTO `comments` VALUES (1,1,'Re: New Media','2009-12-01 19:43:50','2009-12-01 19:43:50',1,1,'John Doe',NULL,2130706433,'<p>Hello to you too!</p>');
 /*!40000 ALTER TABLE `comments` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `groups`
+--
+
+DROP TABLE IF EXISTS `groups`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `groups` (
+  `group_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `group_name` varchar(16) NOT NULL,
+  `display_name` varchar(255) DEFAULT NULL,
+  `created` datetime DEFAULT NULL,
+  PRIMARY KEY (`group_id`),
+  UNIQUE KEY `group_name` (`group_name`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `groups`
+--
+
+LOCK TABLES `groups` WRITE;
+/*!40000 ALTER TABLE `groups` DISABLE KEYS */;
+INSERT INTO `groups` VALUES (1,'Admins','Administrators','2009-05-21 18:41:28');
+/*!40000 ALTER TABLE `groups` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `groups_permissions`
+--
+
+DROP TABLE IF EXISTS `groups_permissions`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `groups_permissions` (
+  `group_id` int(10) unsigned DEFAULT NULL,
+  `permission_id` int(10) unsigned DEFAULT NULL,
+  KEY `group_id` (`group_id`),
+  KEY `permission_id` (`permission_id`),
+  CONSTRAINT `groups_permissions_ibfk_1` FOREIGN KEY (`group_id`) REFERENCES `groups` (`group_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `groups_permissions_ibfk_2` FOREIGN KEY (`permission_id`) REFERENCES `permissions` (`permission_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `groups_permissions`
+--
+
+LOCK TABLES `groups_permissions` WRITE;
+/*!40000 ALTER TABLE `groups_permissions` DISABLE KEYS */;
+INSERT INTO `groups_permissions` VALUES (1,1);
+/*!40000 ALTER TABLE `groups_permissions` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -158,7 +212,6 @@ CREATE TABLE `media` (
   `description_plain` text,
   `notes` text,
   `duration` int(10) unsigned NOT NULL,
-  `player_override` varchar(50) DEFAULT NULL,
   `views` int(10) unsigned NOT NULL DEFAULT '0',
   `likes` int(10) unsigned NOT NULL DEFAULT '0',
   `popularity_points` int(10) unsigned NOT NULL DEFAULT '0',
@@ -177,7 +230,7 @@ CREATE TABLE `media` (
 
 LOCK TABLES `media` WRITE;
 /*!40000 ALTER TABLE `media` DISABLE KEYS */;
-INSERT INTO `media` VALUES (1,NULL,'new-media',0,0,0,NULL,'2009-12-01 19:40:23','2009-12-01 19:43:34',NULL,NULL,'New Media',NULL,'<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>',NULL,'',0,NULL,1,0,0,'Admin','admin@localhost');
+INSERT INTO `media` VALUES (1,NULL,'new-media',0,0,0,NULL,'2009-12-01 19:40:23','2009-12-01 19:43:34',NULL,NULL,'New Media',NULL,'<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>',NULL,'',0,1,0,0,'Admin','admin@localhost');
 /*!40000 ALTER TABLE `media` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!50003 SET @saved_cs_client      = @@character_set_client */ ;
@@ -335,16 +388,13 @@ DROP TABLE IF EXISTS `media_files`;
 CREATE TABLE `media_files` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `media_id` int(10) unsigned NOT NULL,
-  `type` varchar(10) CHARACTER SET ascii NOT NULL,
-  `url` varchar(255) CHARACTER SET ascii NOT NULL,
+  `type` enum('video','audio','audio_desc','captions') NOT NULL,
+  `container` varchar(10) CHARACTER SET ascii NOT NULL,
+  `display_name` varchar(255) NOT NULL,
+  `file_name` varchar(255) DEFAULT NULL,
+  `url` varchar(255) CHARACTER SET ascii DEFAULT NULL,
+  `embed` varchar(50) DEFAULT NULL,
   `size` int(10) unsigned DEFAULT NULL,
-  `width` smallint(5) unsigned DEFAULT NULL,
-  `height` smallint(5) unsigned DEFAULT NULL,
-  `bitrate` smallint(5) unsigned DEFAULT NULL,
-  `position` tinyint(3) unsigned NOT NULL DEFAULT '0',
-  `enable_player` tinyint(1) NOT NULL DEFAULT '1',
-  `enable_feed` tinyint(1) NOT NULL DEFAULT '1',
-  `is_original` tinyint(1) NOT NULL DEFAULT '0',
   `created_on` datetime NOT NULL,
   `modified_on` datetime NOT NULL,
   PRIMARY KEY (`id`),
@@ -467,6 +517,32 @@ DELIMITER ;
 /*!50003 SET collation_connection  = @saved_col_connection */ ;
 
 --
+-- Table structure for table `permissions`
+--
+
+DROP TABLE IF EXISTS `permissions`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `permissions` (
+  `permission_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `permission_name` varchar(16) NOT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`permission_id`),
+  UNIQUE KEY `permission_name` (`permission_name`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `permissions`
+--
+
+LOCK TABLES `permissions` WRITE;
+/*!40000 ALTER TABLE `permissions` DISABLE KEYS */;
+INSERT INTO `permissions` VALUES (1,'admin','Grants access to the admin panel');
+/*!40000 ALTER TABLE `permissions` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `podcasts`
 --
 
@@ -516,7 +592,7 @@ CREATE TABLE `settings` (
   `value` text,
   PRIMARY KEY (`id`),
   UNIQUE KEY `key_index` (`key`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -525,17 +601,32 @@ CREATE TABLE `settings` (
 
 LOCK TABLES `settings` WRITE;
 /*!40000 ALTER TABLE `settings` DISABLE KEYS */;
-INSERT INTO `settings` VALUES (1,'email_media_uploaded',NULL),
-(2,'email_comment_posted',NULL),
-(3,'email_support_requests',NULL),
-(4,'email_send_from','noreply@localhost'),
-(5,'wording_user_uploads','Upload your media using the form below. We\'ll review it and get back to you.'),
-(6,'wording_additional_notes',NULL),
-(7,'rich_text_editor','tinymce'),
-(8,'popularity_decay_exponent','4'),
-(9,'popularity_decay_lifetime','36'),
-(10,'player','flowplayer'),
-(10,'featured_category','1');
+INSERT INTO `settings` VALUES
+(NULL,'email_media_uploaded',NULL),
+(NULL,'email_comment_posted',NULL),
+(NULL,'email_support_requests',NULL),
+(NULL,'email_send_from','noreply@localhost'),
+(NULL,'wording_user_uploads','Upload your media using the form below. We\'ll review it and get back to you.'),
+(NULL,'wording_additional_notes',NULL),
+(NULL,'popularity_decay_exponent','4'),
+(NULL,'popularity_decay_lifetime','36'),
+(NULL,'rich_text_editor','tinymce'),
+(NULL,'google_analytics_uacct',''),
+(NULL,'flash_player','flowplayer'),
+(NULL,'html5_player','html5'),
+(NULL,'player_type','best'),
+(NULL,'featured_category','1'),
+(NULL, 'max_upload_size', '314572800'),
+(NULL, 'ftp_storage', 'false'),
+(NULL, 'ftp_server', 'ftp.someserver.com'),
+(NULL, 'ftp_user', 'username'),
+(NULL, 'ftp_password', 'password'),
+(NULL, 'ftp_upload_directory', 'media'),
+(NULL, 'ftp_download_url', 'http://www.someserver.com/web/accessible/media/'),
+(NULL, 'ftp_upload_integrity_retries', '10'),
+(NULL, 'akismet_key',''),
+(NULL, 'akismet_url',''),
+(NULL, 'req_comment_approval', 'false');
 /*!40000 ALTER TABLE `settings` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -619,93 +710,13 @@ DELIMITER ;
 /*!50003 SET collation_connection  = @saved_col_connection */ ;
 
 --
--- Table structure for table `tg_group`
+-- Table structure for table `users`
 --
 
-DROP TABLE IF EXISTS `tg_group`;
+DROP TABLE IF EXISTS `users`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `tg_group` (
-  `group_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `group_name` varchar(16) NOT NULL,
-  `display_name` varchar(255) DEFAULT NULL,
-  `created` datetime DEFAULT NULL,
-  PRIMARY KEY (`group_id`),
-  UNIQUE KEY `group_name` (`group_name`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `tg_group`
---
-
-LOCK TABLES `tg_group` WRITE;
-/*!40000 ALTER TABLE `tg_group` DISABLE KEYS */;
-INSERT INTO `tg_group` VALUES (1,'Admins','Administrators','2009-05-21 18:41:28');
-/*!40000 ALTER TABLE `tg_group` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `tg_group_permission`
---
-
-DROP TABLE IF EXISTS `tg_group_permission`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `tg_group_permission` (
-  `group_id` int(10) unsigned DEFAULT NULL,
-  `permission_id` int(10) unsigned DEFAULT NULL,
-  KEY `group_id` (`group_id`),
-  KEY `permission_id` (`permission_id`),
-  CONSTRAINT `tg_group_permission_ibfk_1` FOREIGN KEY (`group_id`) REFERENCES `tg_group` (`group_id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `tg_group_permission_ibfk_2` FOREIGN KEY (`permission_id`) REFERENCES `tg_permission` (`permission_id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `tg_group_permission`
---
-
-LOCK TABLES `tg_group_permission` WRITE;
-/*!40000 ALTER TABLE `tg_group_permission` DISABLE KEYS */;
-INSERT INTO `tg_group_permission` VALUES (1,1);
-/*!40000 ALTER TABLE `tg_group_permission` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `tg_permission`
---
-
-DROP TABLE IF EXISTS `tg_permission`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `tg_permission` (
-  `permission_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `permission_name` varchar(16) NOT NULL,
-  `description` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`permission_id`),
-  UNIQUE KEY `permission_name` (`permission_name`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `tg_permission`
---
-
-LOCK TABLES `tg_permission` WRITE;
-/*!40000 ALTER TABLE `tg_permission` DISABLE KEYS */;
-INSERT INTO `tg_permission` VALUES (1,'admin','Grants access to the admin panel');
-/*!40000 ALTER TABLE `tg_permission` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `tg_user`
---
-
-DROP TABLE IF EXISTS `tg_user`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `tg_user` (
+CREATE TABLE `users` (
   `user_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `user_name` varchar(16) NOT NULL,
   `email_address` varchar(255) NOT NULL,
@@ -719,40 +730,40 @@ CREATE TABLE `tg_user` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `tg_user`
+-- Dumping data for table `users`
 --
 
-LOCK TABLES `tg_user` WRITE;
-/*!40000 ALTER TABLE `tg_user` DISABLE KEYS */;
-INSERT INTO `tg_user` VALUES (1,'admin','admin@localhost','Admin','80aa2676d7501e68547d0b9a75438e94ff9b7509916fb3b7bcd7fe630a4f1bed836e82dc5eded90e','2009-12-01 19:25:12');
-/*!40000 ALTER TABLE `tg_user` ENABLE KEYS */;
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'admin','admin@localhost','Admin','80aa2676d7501e68547d0b9a75438e94ff9b7509916fb3b7bcd7fe630a4f1bed836e82dc5eded90e','2009-12-01 19:25:12');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table `tg_user_group`
+-- Table structure for table `users_groups`
 --
 
-DROP TABLE IF EXISTS `tg_user_group`;
+DROP TABLE IF EXISTS `users_groups`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `tg_user_group` (
+CREATE TABLE `users_groups` (
   `user_id` int(10) unsigned DEFAULT NULL,
   `group_id` int(10) unsigned DEFAULT NULL,
   KEY `user_id` (`user_id`),
   KEY `group_id` (`group_id`),
-  CONSTRAINT `tg_user_group_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `tg_user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `tg_user_group_ibfk_2` FOREIGN KEY (`group_id`) REFERENCES `tg_group` (`group_id`) ON DELETE CASCADE ON UPDATE CASCADE
+  CONSTRAINT `users_groups_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `users_groups_ibfk_2` FOREIGN KEY (`group_id`) REFERENCES `groups` (`group_id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `tg_user_group`
+-- Dumping data for table `users_groups`
 --
 
-LOCK TABLES `tg_user_group` WRITE;
-/*!40000 ALTER TABLE `tg_user_group` DISABLE KEYS */;
-INSERT INTO `tg_user_group` VALUES (1,1);
-/*!40000 ALTER TABLE `tg_user_group` ENABLE KEYS */;
+LOCK TABLES `users_groups` WRITE;
+/*!40000 ALTER TABLE `users_groups` DISABLE KEYS */;
+INSERT INTO `users_groups` VALUES (1,1);
+/*!40000 ALTER TABLE `users_groups` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -764,4 +775,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2010-04-01 16:02:03
+-- Dump completed on 2010-04-28 21:04:54
