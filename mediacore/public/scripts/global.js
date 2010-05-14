0680e59769e8688304546f22685bd9d89d3028e0
Nathan Wright <nathan@simplestation.com>
2010-05-14 11:44:51 -0700

    Simplify 'like this' js

--- mediacore/public/scripts/global.js
+++ mediacore/public/scripts/global.js
@@ -4,16 +4,13 @@ window.addEvent('domready', function(){
 });
 
 window.addEvent('domready', function(){
-	var likeBtns = $$('a.meta-likes');
-	if (likeBtns) {
-		likeBtns.addEvent('click', function(e){
-			var btn = new Event(e).stop().target;
-			if (btn.get('tag') != 'a') btn = btn.getParent('a');
-			var req = new Request({url: btn.get('href')}).addEvent('complete', function(resp){
-				if (resp) new Element('span', {'html': resp.toInt() + ' <strong>Like this</strong>', 'class': btn.className}).replaces(btn);
-			}).send();
-		});
-	}
+	var likeBtns = $$('a.meta-likes').addEvent('click', function(e){
+		var btn = new Event(e).stop().target;
+		if (btn.get('tag') != 'a') btn = btn.getParent('a');
+		var req = new Request({url: btn.get('href'), onComplete: function(resp){
+			if (resp) new Element('span', {'html': resp.toInt() + ' <strong>Like this</strong>', 'class': btn.className}).replaces(btn);
+		}}).send();
+	});
 });
 
 var MetaHover = new Class({
