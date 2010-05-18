6ded8a92f58a3ffe62eaf6c2652f4648e2cff9b0
Felix Schwarz <felix.schwarz@oss.schwarz.eu>
2010-05-18 08:11:11 +0200

    fix websetup.py to make it compatible with Pylons

--- mediacore/websetup.py
+++ mediacore/websetup.py
@@ -1,10 +1,11 @@
 """Setup the MediaCore application"""
 import logging
 
+import pylons
 import transaction
-from pylons import config
 
 from mediacore.config.environment import load_environment
+from mediacore.model import DBSession, metadata
 
 log = logging.getLogger(__name__)
 
@@ -15,13 +16,11 @@ def setup_app(command, conf, vars):
         load_environment(conf.global_conf, conf.local_conf)
 
     # Load the models
-    from mediacore import model
     print "Creating tables"
-    model.metadata.create_all(bind=config['pylons.app_globals'].sa_engine)
+    metadata.create_all(bind=DBSession.bind)
 
     # Set up any useful initial data here.
     # TODO: Use this file to replace setup.sql
-
-    model.DBSession.flush()
+    DBSession.flush()
     transaction.commit()
     print "Successfully setup"
