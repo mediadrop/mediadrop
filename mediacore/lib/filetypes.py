mediadrop/mediadrop

commit cc9bcfe16140dbf49b6b048bd0cbfbf2679c4f2d

Add oga, ogv, mka, mkv filetypes

mediacore/lib/filetypes.py
@@ -44,9 +44,13 @@ mimetype_lookup = {
     'flac': 'audio/flac',
     'mp3':  'audio/mpeg',
     'mp4':  '%s/mp4',
-    'm4a':  '%s/mp4',
-    'm4v':  '%s/mp4',
+    'm4a':  'audio/mp4',
+    'm4v':  'video/mp4',
     'ogg':  '%s/ogg',
+    'oga':  'audio/ogg',
+    'ogv':  'video/ogg',
+    'mka':  'audio/x-matroska',
+    'mkv':  'video/x-matroska',
     '3gp':  '%s/3gpp',
     '3g2':  '%s/3gpp',
     'divx': 'video/mpeg',
@@ -70,6 +74,7 @@ container_lookup = {
     'audio/mp4': 'mp4',
     'audio/mpeg': 'mp3',
     'audio/ogg': 'ogg',
+    'audio/x-matroska': 'mka',
     'video/3gpp': '3gp',
     'video/mp4': 'mp4',
     'video/mpeg': 'mpg',
@@ -77,6 +82,7 @@ container_lookup = {
     'video/quicktime': 'mov',
     'video/x-dv': 'dv',
     'video/x-flv': 'flv',
+    'video/x-matroska': 'mkv',
     'video/x-ms-wmv': 'wmv',
     'video/x-vob': 'vob',
     'application/ttml+xml': 'xml',

commit 5dc244d3b72891ff3fa502c197056b115e1dcdfa

Quickfix for Flash crashing on upload: removing some format support
Introduced by cc9bcfe16140dbf49b6b048bd0cbfbf2679c4f2d

For some reason Flash will crash if you give the type filter more than
21 file extensions to use. This gets us back down to that number, but
we'll want a better way of dealing with this.

mediacore/lib/filetypes.py
@@ -40,6 +40,9 @@ CAPTIONS = 'captions'
 #      and sometimes treated as file extensions. Caveat coder.
 # TODO: Replace with a more complete list or (even better) change the logic
 #       to detect mimetypes from something other than the file extension.
+# XXX: This list can contain at most 21 items! Any more will crash Flash when
+#      accepted_extensions() is passed to Swiff.Uploader to provide the
+#      typeFilter.
 mimetype_lookup = {
     'flac': 'audio/flac',
     'mp3':  'audio/mpeg',
@@ -55,13 +58,10 @@ mimetype_lookup = {
     '3g2':  '%s/3gpp',
     'divx': 'video/mpeg',
     'dv':   'video/x-dv',
-    'dvx':  'video/mpeg',
     'flv':  'video/x-flv', # made up, it's what everyone uses anyway.
     'mov':  'video/quicktime',
     'mpeg': '%s/mpeg',
     'mpg':  '%s/mpeg',
-    'qt':   'video/quicktime',
-    'vob':  '%s/x-vob', # multiplexed container format
     'wmv':  'video/x-ms-wmv',
     'xml':  'application/ttml+xml',
     'srt':  'text/plain',