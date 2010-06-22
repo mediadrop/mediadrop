77e4eeeb77bcbab034e407b1ab745373419b8683
Nathan Wright <nathan@simplestation.com>
2010-06-22 03:39:29 -0700

    Fix a KeyError when auth encounters a client with no HTTP_USER_AGENT

--- mediacore/lib/auth.py
+++ mediacore/lib/auth.py
@@ -64,16 +64,17 @@ def add_auth(app, config):
 def classifier_for_flash_uploads(environ):
     """Normally classifies the request as browser, dav or xmlpost.
 
-    When the Flash uploader is sending a file, it appends the authtkt session ID
-    to the POST data so we spoof the cookie header so that the auth code will
-    think this was a normal request. In the process, we overwrite any
+    When the Flash uploader is sending a file, it appends the authtkt session
+    ID to the POST data so we spoof the cookie header so that the auth code
+    will think this was a normal request. In the process, we overwrite any
     pseudo-cookie data that is sent by Flash.
 
     TODO: Currently overwrites the HTTP_COOKIE, should ideally append.
     """
     classification = default_request_classifier(environ)
-    if classification == 'browser' and environ['REQUEST_METHOD'] == 'POST'\
-                                   and 'Flash' in environ['HTTP_USER_AGENT']:
+    if classification == 'browser' \
+    and environ['REQUEST_METHOD'] == 'POST' \
+    and 'Flash' in environ.get('HTTP_USER_AGENT'):
         try:
             session_key = environ['repoze.who.plugins']['cookie'].cookie_name
             session_id = parse_formvars(environ)[session_key]
