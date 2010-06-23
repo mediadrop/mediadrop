71aa5998bbec49b84ca35ef18abf9641ef261a0f
Anthony Theocharis <anthony@simplestation.com>
2010-06-23 15:22:37 -0700

    Add a test for non-forced logins.

--- mediacore/tests/functional/test_login.py
+++ mediacore/tests/functional/test_login.py
@@ -42,3 +42,53 @@ class TestLoginController(TestController):
 
         # Follow the redirect to check that we were correctly authenticated:
         initial_page = post_login_handler_page.follow(status=200)
+
+    def test_voluntary_login_and_logout(self):
+        """
+        Voluntary logins should redirect to the main admin page on
+        success. Logout should redirect to the main / page.
+        """
+        admin_url = url(controller='admin', action='index')
+        login_form_url = url(controller='login', action='login')
+        post_login_url = url(controller='login', action='post_login')
+        logout_handler_url = url(controller='login', action='logout_handler')
+        post_logout_url = url(controller='login', action='post_logout')
+        home_url = url('/')
+
+        # Request, and fill out, the login form.
+        login_page = self.app.get(login_form_url, status=200)
+        login_page.form['login'] = test_user
+        login_page.form['password'] = test_password
+
+        # Submitting the login form should redirect us to the 'post_login' page
+        login_handler_page = login_page.form.submit(status=302)
+        assert login_handler_page.location.startswith(
+                    'http://localhost%s' % post_login_url)
+
+        # The post_login page should set up our authentication cookies
+        # and redirect to the initially requested page.
+        post_login_handler_page = login_handler_page.follow(status=302)
+        assert post_login_handler_page.location == \
+                'http://localhost%s' % admin_url
+        assert 'authtkt' in post_login_handler_page.request.cookies, \
+               "Session cookie wasn't defined: %s" % post_login_handler_page.request.cookies
+
+        # Follow the redirect to check that we were correctly authenticated:
+        admin_page = post_login_handler_page.follow(status=200)
+
+        # Now 'click' the logout link.
+        # This sends all relevant cookies, and ensures that the logout link is
+        # actually displayed on the admin page.
+        logout_handler_page = admin_page.click(linkid='logout', href=logout_handler_url)
+        assert logout_handler_page.location.startswith(
+                'http://localhost%s' % post_logout_url)
+
+        # Follow the first logout redirect.
+        # This should invalidate our authtkt cookie.
+        post_logout_page = logout_handler_page.follow(status=302)
+        assert post_logout_page.location == \
+                'http://localhost%s' % home_url
+        assert post_logout_page.request.cookies['authtkt'] == 'INVALID'
+
+        # Follow the final logout redirect, back to the home page.
+        home_page = post_logout_page.follow(status=200)
