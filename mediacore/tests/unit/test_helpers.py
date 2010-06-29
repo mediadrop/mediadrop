60691b692d3484fb19f4df777de5e4385be7d27b
Anthony Theocharis <anthony@simplestation.com>
2010-06-28 23:24:50 -0700

    Add more tests for correct selection of file to serve.

--- mediacore/tests/unit/test_helpers.py
+++ mediacore/tests/unit/test_helpers.py
@@ -138,3 +138,111 @@ class TestHelpers(TestController):
             assert player == players[e_player], "Expected %r but was %r" % (players[e_player], player)
             assert file == media_files[e_file], "Expected %r but got %r" % (media_files[e_file], file)
 
+    def test_sized_file_picking(self):
+        """Test while file gets served to each browser, given that all files
+        are the same filesize.
+        """
+        media, media_files = self._get_media('sized')
+
+        media_files['youtube'].size = None
+        media_files['flv'].size = 500
+        media_files['oga'].size = 1000
+        media_files['ogv'].size = 2000
+        media_files['m4a'].size = 3000
+        media_files['m4v'].size = 4000
+        media_files['mp3'].size = 5000
+        media_files['xml'].size = 6000
+
+        pylons.app_globals.settings['html5_player'] = 'html5'
+        pylons.app_globals.settings['flash_player'] = 'flowplayer'
+
+        combinations = [
+            # Prefer flash, without embeds
+            ('firefox', 2,          'flash', False, 'm4v', 'flowplayer'),
+            ('firefox', 3,          'flash', False, 'm4v', 'flowplayer'),
+            ('firefox', 3.5,        'flash', False, 'm4v', 'flowplayer'),
+            ('safari', 522,         'flash', False, 'm4v', 'flowplayer'),
+            ('opera', 10.5,         'flash', False, 'm4v', 'flowplayer'),
+            ('opera', 9,            'flash', False, 'm4v', 'flowplayer'),
+            ('chrome', 3.0,         'flash', False, 'm4v', 'flowplayer'),
+            ('android', 0,          'flash', False, 'm4v', 'flowplayer'),
+            ('itunes', 0,           'flash', False, 'm4v', 'html5'),
+            ('iphone-ipod-ipad', 0, 'flash', False, 'm4v', 'html5'),
+            ('unknown', 0,          'flash', False, 'm4v', 'flowplayer'),
+            # Prefer flash, including embeds
+            ('firefox', 2,          'flash', True, 'youtube', 'youtube'),
+            ('firefox', 3,          'flash', True, 'youtube', 'youtube'),
+            ('firefox', 3.5,        'flash', True, 'youtube', 'youtube'),
+            ('safari', 522,         'flash', True, 'youtube', 'youtube'),
+            ('opera', 10.5,         'flash', True, 'youtube', 'youtube'),
+            ('opera', 9,            'flash', True, 'youtube', 'youtube'),
+            ('chrome', 3.0,         'flash', True, 'youtube', 'youtube'),
+            ('android', 0,          'flash', True, 'youtube', 'youtube'),
+            ('itunes', 0,           'flash', True, 'm4v',     'html5'),
+            ('iphone-ipod-ipad', 0, 'flash', True, 'm4v',     'html5'),
+            ('unknown', 0,          'flash', True, 'youtube', 'youtube'),
+            # Prefer HTML5, without embeds
+            ('firefox', 2,          'best', False, 'm4v', 'flowplayer'),
+            ('firefox', 3,          'best', False, 'm4v', 'flowplayer'),
+            ('firefox', 3.5,        'best', False, 'ogv', 'html5'),
+            ('safari', 522,         'best', False, 'm4v', 'html5'),
+            ('opera', 10.5,         'best', False, 'ogv', 'html5'),
+            ('opera', 9,            'best', False, 'm4v', 'flowplayer'),
+            ('chrome', 3.0,         'best', False, 'm4v', 'html5'),
+            ('android', 0,          'best', False, 'm4v', 'html5'),
+            ('itunes', 0,           'best', False, 'm4v', 'html5'),
+            ('iphone-ipod-ipad', 0, 'best', False, 'm4v', 'html5'),
+            ('unknown', 0,          'best', False, 'm4v', 'flowplayer'),
+            # Prefer HTML5, including embeds
+            ('firefox', 2,          'best', True, 'youtube', 'youtube'),
+            ('firefox', 3,          'best', True, 'youtube', 'youtube'),
+            ('firefox', 3.5,        'best', True, 'ogv',     'html5'),
+            ('safari', 522,         'best', True, 'm4v',     'html5'),
+            ('opera', 10.5,         'best', True, 'ogv',     'html5'),
+            ('opera', 9,            'best', True, 'youtube', 'youtube'),
+            ('chrome', 3.0,         'best', True, 'm4v',     'html5'),
+            ('android', 0,          'best', True, 'm4v',     'html5'),
+            ('itunes', 0,           'best', True, 'm4v',     'html5'),
+            ('iphone-ipod-ipad', 0, 'best', True, 'm4v',     'html5'),
+            ('unknown', 0,          'best', True, 'youtube', 'youtube'),
+            # HTML5 only, without embeds
+            ('firefox', 2,          'html5', False, None,  None),
+            ('firefox', 3,          'html5', False, None,  None),
+            ('firefox', 3.5,        'html5', False, 'ogv', 'html5'),
+            ('safari', 522,         'html5', False, 'm4v', 'html5'),
+            ('opera', 10.5,         'html5', False, 'ogv', 'html5'),
+            ('opera', 9,            'html5', False, None,  None),
+            ('chrome', 3.0,         'html5', False, 'm4v', 'html5'),
+            ('android', 0,          'html5', False, 'm4v', 'html5'),
+            ('itunes', 0,           'html5', False, 'm4v', 'html5'),
+            ('iphone-ipod-ipad', 0, 'html5', False, 'm4v', 'html5'),
+            ('unknown', 0,          'html5', False, None, None),
+            # HTML5 only, including embeds
+            ('firefox', 2,          'html5', True, None,  None),
+            ('firefox', 3,          'html5', True, None,  None),
+            ('firefox', 3.5,        'html5', True, 'ogv', 'html5'),
+            ('safari', 522,         'html5', True, 'm4v', 'html5'),
+            ('opera', 10.5,         'html5', True, 'ogv', 'html5'),
+            ('opera', 9,            'html5', True, None,  None),
+            ('chrome', 3.0,         'html5', True, 'm4v', 'html5'),
+            ('android', 0,          'html5', True, 'm4v', 'html5'),
+            ('itunes', 0,           'html5', True, 'm4v', 'html5'),
+            ('iphone-ipod-ipad', 0, 'html5', True, 'm4v', 'html5'),
+            ('unknown', 0,          'html5', True, None, None),
+        ]
+
+        from mediacore.lib.helpers import players
+        players = dict(players)
+        players[None] = None
+        media_files[None] = None
+
+        for browser, version, p_type, embedded, e_file, e_player in combinations:
+            file, player, browser, version = pick_media_file_player(media.files,
+                    browser = browser,
+                    version = version,
+                    player_type = p_type,
+                    include_embedded = embedded
+            )
+            print "Sized:", browser, version, p_type, embedded, e_file, e_player
+            assert player == players[e_player], "Expected %r but was %r" % (players[e_player], player)
+            assert file == media_files[e_file], "Expected %r but got %r" % (media_files[e_file], file)
