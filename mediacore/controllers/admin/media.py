mediadrop/mediadrop

commit 379c4fc10e0541500c652ecd6818ff98b9c2de6a

Only show human-readable exceptions when file uploads fail

mediacore/controllers/admin/media.py
@@ -297,7 +297,7 @@ class MediaController(BaseController):
         message = None
         try:
             media_file = add_new_media_file(media, file, url)
-        except Exception, e:
+        except Invalid, e:
             DBSession.rollback()
             message = e.message
 

commit e4a3e7e1addcf87254930cc3f5c32a34fb411430

Fix missing import.
bug introduced in 379c4fc10e0541500c652ecd6818ff98b9c2de6a

mediacore/controllers/admin/media.py
@@ -20,7 +20,7 @@ import filecmp
 import os
 from datetime import datetime
 
-from formencode import validators
+from formencode import Invalid, validators
 from pylons import config, request, response, session, tmpl_context
 from pylons.i18n import _
 from repoze.what.predicates import has_permission