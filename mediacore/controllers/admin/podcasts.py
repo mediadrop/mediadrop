mediadrop/mediadrop

commit 1d22d15d331df50d9edc4f3e3065dab857e82cba

Show the feed URL in the admin podcast edit interface

mediacore/controllers/admin/podcasts.py
@@ -91,8 +91,10 @@ class PodcastsController(BaseController):
             user = request.environ['repoze.who.identity']['user']
             form_values.setdefault('author_name', user.display_name)
             form_values.setdefault('author_email', user.email_address)
+            form_values.setdefault('feed', {}).setdefault('feed_url',
+                u'Save the media to get your feed URL')
         else:
-            explicit_values = dict(yes=True, clean=False)
+            explicit_values = {True: 'yes', False: 'clean', None: 'no'}
             form_values = dict(
                 slug = podcast.slug,
                 title = podcast.title,
@@ -101,9 +103,13 @@ class PodcastsController(BaseController):
                 author_email = podcast.author and podcast.author.email or None,
                 description = podcast.description,
                 details = dict(
-                    explicit = {True: 'yes', False: 'clean'}.get(podcast.explicit, 'no'),
+                    explicit = explicit_values.get(podcast.explicit),
                     category = podcast.category,
                     copyright = podcast.copyright,
+                ),
+                feed = dict(
+                    feed_url = url_for(controller='/podcasts', action='feed',
+                                       slug=podcast.slug, qualified=True),
                     itunes_url = podcast.itunes_url,
                     feedburner_url = podcast.feedburner_url,
                 ),

mediacore/forms/admin/podcasts.py
@@ -110,6 +110,9 @@ class PodcastForm(ListForm):
             SingleSelectField('explicit', label_text='Explicit?', options=explicit_options),
             SingleSelectField('category', options=category_options),
             TextField('copyright', maxlength=50),
+        ]),
+        ListFieldSet('feed', suppress_label=True, legend='Advanced Options:', css_classes=['details_fieldset'], children=[
+            TextField('feed_url', maxlength=50, label_text='Your Feed URL', attrs={'readonly': True}),
             TextField('itunes_url', validator=URL, label_text='iTunes URL', help_text=Markup('<a href="https://phobos.apple.com/WebObjects/MZFinance.woa/wa/publishPodcast" target="_blank">Get an iTunes URL</a>'), maxlength=80),
             TextField('feedburner_url', validator=URL, label_text='Feedburner URL', help_text=Markup('<a href="http://feedburner.com/" target="_blank">Get a Feedburner URL</a>'), maxlength=80),
         ]),

mediacore/public/admin/styles/podcasts.css
@@ -92,11 +92,15 @@
 #podcast-form .details_fieldset li {
 	clear: both;
 }
-#details_itunes_url,
-#details_feedburner_url,
+#feed_feed_url,
+#feed_itunes_url,
+#feed_feedburner_url,
 #details_copyright {
 	width: 285px;
 	float: left;
+}
+#feed_feed_url {
+	color: #5D7485;
 }
 #podcast-form #details_explicit li {
 	float: left;

mediacore/templates/admin/podcasts/edit.html
@@ -35,6 +35,9 @@
 		window.addEvent('domready', function(){
 			var slug = new BoxForm.Slug('slug', {slugify: 'title'});
 			if (${int(podcast.id is not None)}) slug.detachSlugifier();
+			$('feed_feed_url').addEvent('click', function(e){
+				e.target.select();
+			});
 
 			var deleteConfirm = new DeleteConfirmMgr({msg: 'Are you sure you want to delete this podcast?'});
 			$('delete').addEvent('click', deleteConfirm.openConfirmDialog.bind(deleteConfirm));

mediacore/templates/admin/podcasts/form.html
@@ -1,77 +1,0 @@
-<!--! This file is a part of MediaCore, Copyright 2009 Simple Station Inc.
-
-	MediaCore is free software: you can redistribute it and/or modify
-	it under the terms of the GNU General Public License as published by
-	the Free Software Foundation, either version 3 of the License, or
-	(at your option) any later version.
-
-	MediaCore is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-	GNU General Public License for more details.
-
-	You should have received a copy of the GNU General Public License
-	along with this program.  If not, see <http://www.gnu.org/licenses/>.
--->
-<form xmlns="http://www.w3.org/1999/xhtml"
-      xmlns:py="http://genshi.edgewall.org/"
-      xmlns:xi="http://www.w3.org/2001/XInclude"
-      id="${id}"
-      name="${name}"
-      action="${action}"
-      method="${method}"
-      class="${css_class}"
-      py:attrs="attrs">
-	<div class="box-content">
-		<div class="field-error" py:if="error and show_error" py:content="error" />
-		<div py:if="hidden_fields">
-			<div py:for="field in hidden_fields"
-				 py:with="error=error_for(field)"
-				 py:strip="True">
-				<span py:replace="field.display(value_for(field), **args_for(field))" />
-				<span py:if="show_children_errors and error and not field.show_error"
-					  class="field-error" py:content="error" />
-			</div>
-		</div>
-		<ul class="field-list" py:attrs="list_attrs">
-			<li py:for="i, field in enumerate(fields)"
-				py:if="getattr(field, 'type', None) not in ['submit', 'file']"
-				py:with="required=[None,' required'][int(field.is_required)];
-						 error=error_for(field);
-						 label_text = field.label_text;
-						 help_text = field.help_text;
-						 is_submit = getattr(field, 'type', None) == 'submit';"
-				id="${field.id}-container"
-				class="${i%2 and 'odd' or 'even'}${is_submit and ' submit' or None}"
-				py:attrs="args_for(field).get('container_attrs') or field.container_attrs">
-				<div py:if="show_labels and label_text and not field.suppress_label"
-					 id="${field.id}-label" class="form-label">
-					<label for="${field.id}">${label_text}:</label>
-				</div>
-				<div class="form-field">
-					${field.display(value_for(field), **args_for(field))}
-				</div>
-				<div py:if="field.name == 'description'" py:replace="xhtml_description('description')" />
-				<span py:if="help_text" class="field-help" py:content="help_text" />
-				<span py:if="show_children_errors and error and not field.show_error"
-					  class="field-error" py:content="error" />
-			</li>
-		</ul>
-		<span py:if="error and not error.error_dict" class="field-error" py:content="error" />
-	</div>
-<!--!	<div id="thumb-wrapper" class="box fitted-sbox">
-		<h1 class="box-head">Thumb</h1>
-		<div class="box-content">
-			<p>Select an image to upload:</p>
-			${XML(children['thumb'])}
-		</div>
-		<div class="box-foot">
-			${XML(children['save'])}
-		</div>
-	</div>-->
-	<div class="box-foot">
-		${XML(children['save'])}
-		${XML(children['delete'])}
-	</div>
-</form>
-

commit 7bf2974d61ac9ac24ab41149b170071ead2fa780

Fix KeyError when saving podcasts
Broken by 1d22d15d331df50d9edc4f3e3065dab857e82cba

mediacore/controllers/admin/podcasts.py
@@ -128,7 +128,7 @@ class PodcastsController(BaseController):
     @expose()
     @validate(podcast_form, error_handler=edit)
     def save(self, id, slug, title, subtitle, author_name, author_email,
-             description, details, delete=None, **kwargs):
+             description, details, feed, delete=None, **kwargs):
         """Save changes or create a new :class:`~mediacore.model.podcasts.Podcast` instance.
 
         Form handler the :meth:`edit` action and the
@@ -156,14 +156,13 @@ class PodcastsController(BaseController):
         podcast.description = description
         podcast.copyright = details['copyright']
         podcast.category = details['category']
-        podcast.itunes_url = details['itunes_url']
-        podcast.feedburner_url = details['feedburner_url']
+        podcast.itunes_url = feed['itunes_url']
+        podcast.feedburner_url = feed['feedburner_url']
         podcast.explicit = {'yes': True, 'clean': False}.get(details['explicit'], None)
 
-        DBSession.add(podcast)
-        DBSession.flush()
-
         if id == 'new':
+            DBSession.add(podcast)
+            DBSession.flush()
             create_default_thumbs_for(podcast)
 
         redirect(action='edit', id=podcast.id)