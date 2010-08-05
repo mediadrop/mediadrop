1c781849915df3fcf6ea49c686c5c8ce3d794608
Nathan Wright <nathan@simplestation.com>
2010-08-05 10:23:10 -0700

    Require Genshi 0.6
    Typically everyone will have this installed already, as it's what
    easy_install would've matched since April 22, 2010.

--- setup.py
+++ setup.py
@@ -1,5 +1,3 @@
-import os
-
 try:
     from setuptools import setup, find_packages
 except ImportError:
@@ -15,7 +13,7 @@ install_requires = [
     'WebHelpers == 1.0',
     'SQLAlchemy >= 0.6.3',
     'sqlalchemy-migrate == 0.6',
-    'Genshi >= 0.5.1',
+    'Genshi == 0.6',
     'Routes == 1.12',
     'repoze.who == 1.0.18',
     'repoze.what-pylons == 1.0',
