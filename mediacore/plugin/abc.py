fdf075f4afd8d281283693b4ff669b45692927e0
Nathan Wright <nathan@simplestation.com>
2010-08-13 16:35:49 -0700

    Implement Abstract Base Classes to improve app extensibility by plugins

---
+++ mediacore/plugin/abc.py
@@ -0,0 +1,111 @@
+# This file is a part of MediaCore, Copyright 2009 Simple Station Inc.
+#
+# MediaCore is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# MediaCore is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import logging
+from collections import defaultdict
+from mediacore import ipython
+
+log = logging.getLogger(__name__)
+
+class AbstractMetaClass(type):
+    """
+    Abstract Class Manager
+
+    This combines concepts from the Trac ComponentMeta class and Python 2.6's abc module.
+
+    http://www.python.org/dev/peps/pep-3119/#specification
+    http://svn.python.org/view/python/trunk/Lib/abc.py?view=markup
+    http://trac.edgewall.org/browser/trunk/trac/core.py#L85
+
+    """
+    _registry = defaultdict(list)
+    _abstracts = {}
+
+    def __new__(mcls, name, bases, namespace):
+        """Create a class object for an abstract class or its implementation.
+
+        For abstract classes, we store the set of abstract attributes
+        that have been defined. We use this data in :meth:`register`
+        to validate all subclasses to ensure it has a complete
+        implementation.
+        """
+        cls = super(AbstractMetaClass, mcls).__new__(mcls, name, bases, namespace)
+        abstracts = set(key for key, value in namespace.iteritems()
+                        if getattr(value, '_isabstract', False))
+        for base in bases:
+            for name in AbstractMetaClass._abstracts.get(base, ''):
+                cls_attr = getattr(cls, name, None)
+                if getattr(cls_attr, '_isabstract', False):
+                    abstracts.add(name)
+        if abstracts:
+            AbstractMetaClass._abstracts[cls] = abstracts
+        return cls
+
+    def register(cls, subclass):
+        """Register an implementation of the abstract class.
+
+        We check the subclass to ensure that it implements all the
+        abstract properties and methods. You do not need to register
+        sub-abstract-classes, just register the complete implementation
+        of the sub-abstract-class.
+
+        :param cls: The abstract class
+        :param subclass: A complete implementation of the abstract class.
+        """
+        # Ensure all abstract methods have been implemented
+        missing = []
+        for name in AbstractMetaClass._abstracts.get(cls, ''):
+            attr = getattr(subclass, name, None)
+            if not attr or getattr(attr, '_isabstract', False):
+                missing.append(name)
+        if missing:
+            raise ImplementationException(
+                'Cannot register %r under %r because it contains abstract '
+                'methods/properties: %s' % (subclass, cls, ', '.join(missing))
+            )
+        # Register the subclass for this abstract class and all its bases
+        for base in cls.__mro__:
+            if base.__class__ is AbstractMetaClass:
+                AbstractMetaClass._registry[base].append(subclass)
+
+    def __iter__(cls):
+        """Iterate over all implementations of the given abstract class."""
+        return iter(AbstractMetaClass._registry[cls])
+
+    def __contains__(cls, subclass):
+        """Return True if the first class is a parent of the second class."""
+        return subclass in AbstractMetaClass._registry[cls]
+
+class AbstractClass(object):
+    """
+    An optional base for abstract classes to subclass.
+    """
+    __metaclass__ = AbstractMetaClass
+
+def abstractmethod(func):
+    func._isabstract = True
+    return func
+
+class abstractproperty(property):
+    _isabstract = True
+
+class ImplementationException(Exception):
+    """
+    Exception raised when a partial abstract class implementation is registered.
+    """
+
+def _reset_registry():
+    AbstractMetaClass._registry.clear()
+    AbstractMetaClass._abstracts.clear()
