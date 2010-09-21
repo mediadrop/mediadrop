59c37cd3fb38c0b030039e759761fdb8edaecb5a
Anthony Theocharis <anthony@simplestation.com>
2010-09-21 12:35:14 -0700

    Remove unused deployment files.
    The new preferred method of restarting the mediacore server is to
    'touch' the .fcgi or .wsgi script.

--- deployment-scripts/mod_wsgi/mediacore-restart.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# If the PID file exists, attempt to kill the identified process.
-PIDFILE="wsgi.pid"
-if [ -f $PIDFILE ]; then
-	kill `cat -- $PIDFILE` && echo "MediaCore successfully stopped" && exit 0
-fi
-# If we got here, there was a problem
-exit 1
