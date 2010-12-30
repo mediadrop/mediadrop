13eded55e3334e795373f79f7650b6b745168ec6
Anthony Theocharis <anthony@simplestation.com>
2010-12-29 17:09:32 -0800

    Add batch script for easy pngcrushing

---
+++ batch-scripts/pngcrush.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+#
+# DESCRIPTION
+# Strips colour profile information, and performs optimal data compression on
+# all PNG images in the directories:
+#     mediacore/public/images
+#     mediacore/public/admin/images
+#
+# DEPENDENCIES
+# This script depends on the common unix utilities:
+#     'find'
+# and the software package:
+#     'pngcrush' -- available via your package manager, or at
+#                   http://pmt.sourceforge.net/pngcrush/
+
+pushd `dirname $0` > /dev/null
+	pushd ../mediacore/public > /dev/null
+		for x in `find images admin/images -iname "*.png"`;
+		do
+			pngcrush -brute -rem gAMA -rem cHRM -rem iCCP -rem sRGB "$x" "$x.crsh";
+			mv "$x.crsh" "$x";
+		done;
+	popd > /dev/null
+popd > /dev/null
