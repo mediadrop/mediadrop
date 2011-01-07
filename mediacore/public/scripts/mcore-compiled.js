mediadrop/mediadrop

commit 04baa7b1ac4b52232a196c6607d28013dfc5cd3c

Don't actually load any video players until the rest of the page has loaded.
Postpones the execution of the player-controller's decorate() method.

This way, nothing in the DOM tree is accessed until it is guaranteed to
be loaded, avoiding intermittent problems with race conditions.

mediacore/public/scripts/mcore/players/controller.js
@@ -63,6 +63,22 @@ mcore.players.Controller.SIZE_COOKIE_NAME = 'mcore-wide';
 
 
 /**
+ * A list of functions, each of which performing the call to
+ * controller.decorate() for a unique controller.
+ * @type{Array.<function()>}
+ */
+mcore.players.Controller.decorateQueue_ = [];
+
+
+/**
+ * Flag that indicates whether the page is loaded and the pageLoaded function
+ * has been called.
+ * @type {boolean}
+ */
+mcore.players.Controller.pageLoaded_ = false;
+
+
+/**
  * Popup instances for download, embed, share, etc.
  * @type {Array.<mcore.popups.SimplePopup>|undefined}
  * @private
@@ -428,25 +444,51 @@ mcore.players.Controller.prototype.disposeInternal = function() {
  * @param {function(mcore.players.Controller)=} opt_callback Optional callback
  *     to allow customization of the newly initialized controller before any
  *     action is taken.
- * @param {number=} opt_delay Optional delay before the controller decorates
- *     the page. Defaults to 25. A delay gives the UI thread an opportunity to
- *     take action elsewhere, reducing the perceived latency of this process.
  * @return {mcore.players.Controller} The new controller instance.
  */
-mcore.players.Controller.init = function(element, player, opt_callback,
-    opt_delay) {
+mcore.players.Controller.init = function(element, player, opt_callback) {
   element = goog.dom.getElement(element);
   var controller = new mcore.players.Controller(player);
-  var delay = goog.isDef(opt_delay) ? opt_delay : 25;
-  if (opt_callback) {
-    opt_callback(controller);
+  var decorateJob = function() {
+    var decorate = function() {
+      controller.decorate(element);
+      if (opt_callback) { opt_callback(controller); }
+    }
+    // Add a delay before the controller decorates the page in order to give
+    // the UI thread an opportunity to take action elsewhere, reducing the
+    // perceived latency of this process.
+    var delay = 25;
+    setTimeout(decorate, delay);
   }
-  setTimeout(function() {
-    controller.decorate(element);
-  }, delay);
+
+  if (mcore.players.Controller.pageLoaded_) {
+    // if the page is loaded, execute the job immediately
+    decorateJob();
+  } else {
+    // otherwise queue the job until the appropriate DOM elements are ready.
+    mcore.players.Controller.decorateQueue_.push(decorateJob);
+  }
+
   return controller;
 };
 
+
+/**
+ * Perform all queued decoration jobs.
+ * Call this after the entire 'media-box' DOM tree has been loaded.
+ */
+mcore.players.Controller.pageLoaded = function() {
+  // Do not execute more than once.
+  if (mcore.players.Controller.pageLoaded_) { return; }
+
+  var queue = mcore.players.Controller.decorateQueue_
+  for (var i=0; i<queue.length; i++) {
+    var decorateJob = queue[i];
+    decorateJob();
+  }
+  mcore.players.Controller.decorateQueue_ = [];
+  mcore.players.Controller.pageLoaded_ = true;
+}
 
 goog.exportSymbol('mcore.initPlayerController', mcore.players.Controller.init);
 goog.exportSymbol('mcore.PlayerController', mcore.players.Controller);

mediacore/public/scripts/mcore/base.js
@@ -58,6 +58,8 @@ mcore.initPage = function() {
       exc.showExcerpt(true);
     }
   }
+
+  mcore.players.Controller.pageLoaded();
 };
 
 

commit 8bf9a69080af2bfdd09c64d7c5e6046ad1762575

Fix javascript and xhtml validation errors on the pop-out media player.
initPage is now required for decoration of the player element to occur.
The optional callback must still be executed before this decoration, however.
These issues were introduced in 04baa7b1ac4b52232a196c6607d28013dfc5cd3c

Also, <head> elements always need <title> elements.

mediacore/public/scripts/mcore-compiled.js
@@ -87,7 +87,7 @@ n.Cc=function(){var a=ye(this);a.Wa(!a.n);a=a.n?"1":"0";var b=void 0;if(/[;=\s]/
 function ze(a){if(a.W){var b=Sb(a.a,Sb(a.a,a.W));if(b){a=ye(a).n;vb(b,"mcore-btn-expand",!a);vb(b,"mcore-btn-shrink",a)}}}n.yc=function(a){a.preventDefault();a=Vb(this.a,a.target,"A");var b=this.Ta||(this.Ta=new le(this.a));me(b,a.href);ne(b,"mcore-likes-counter");Ae(this)};n.tc=function(a){a.preventDefault();a=Vb(this.a,a.target,"A");var b=this.Ta||(this.Ta=new le(this.a));me(b,a.href);ne(b,"mcore-dislikes-counter");Ae(this)};
 function Ae(a){H([a.Oa,a.Fa],function(b){if(b){var c=Sb(this.a,b);(c=c?Sb(this.a,c):j)&&ac(c,0.5);c=this.a.v("DIV",b.className,b.childNodes);var d=b.parentNode;d&&d.replaceChild(c,b);return c}},a);delete a.Oa;delete a.Fa}function ye(a){if(a.wa===undefined){var b;b=(b=a.a.b("media-box"))?sb(b):[];b=G(b,"media-norm")>=0?new Od(a.D):G(b,"media-wide")>=0?new Pd(a.D):j;a.wa=b}return a.wa}n.Yc=function(a){this.wa=a;return this};
 n.Xc=function(a){var b;b=this.a.i.parentWindow||this.a.i.defaultView||window;var c=w(b);b=qe[c]=qe[c]||new oe(b);if(this.Pb=!!a){this.o&&this.D.u(Hb(this.a.i.parentWindow||this.a.i.defaultView||window));P(S(this),b,"resize",this.Kb)}else Xc(S(this),b,"resize",this.Kb);return this};n.Kb=function(a){this.D.u(a.target.O())};n.nc=ba("D");n.d=function(){Z.c.d.call(this);for(var a=0;a<this.Sa.length;++a){this.Sa[a].m();delete this.Sa[a]}jc(this.wa);jc(this.Ta);this.W=this.wa=this.Sa=undefined;delete this.D};
-q("mcore.initPlayerController",function(a,b,c){function d(){setTimeout(function(){f.r(a);c&&c(f)},25)}a=Ab(a);var f=new Z(b);xe?d():we.push(d);return f},void 0);q("mcore.PlayerController",Z,void 0);q("mcore.PlayerController.prototype.setFillScreen",Z.prototype.Xc,void 0);q("mcore.PlayerController.prototype.setResizer",Z.prototype.Yc,void 0);q("mcore.PlayerController.prototype.getPlayer",Z.prototype.nc,void 0);function Be(a,b){R.call(this,b);this.q=a}z(Be,R);n=Be.prototype;n.B=-1;n.k=function(){Be.c.k.call(this);Ce(this)};n.uc=function(a){a.stopPropagation();Ce(this)};function Ce(a){if(a.q[a.B]){a.q[a.B].m();delete a.q[a.B]}var b=a.q[++a.B];if(b){b.ya(a);P(S(a),b,["nosupport","nosupportedsrc"],a.uc);a.sb?b.r(a.b()):b.va(a.b())}else a.dispatchEvent("nosupport")}n.d=function(){Be.c.d.call(this);for(var a=0;a<this.q.length;a++)this.q[a]&&this.q[a].m();delete this.q};n.C=function(){if(this.q[this.B])return this.q[this.B].C()};
+q("mcore.initPlayerController",function(a,b,c){function d(){setTimeout(function(){f.r(a)},25)}a=Ab(a);var f=new Z(b);c&&c(f);xe?d():we.push(d);return f},void 0);q("mcore.PlayerController",Z,void 0);q("mcore.PlayerController.prototype.setFillScreen",Z.prototype.Xc,void 0);q("mcore.PlayerController.prototype.setResizer",Z.prototype.Yc,void 0);q("mcore.PlayerController.prototype.getPlayer",Z.prototype.nc,void 0);function Be(a,b){R.call(this,b);this.q=a}z(Be,R);n=Be.prototype;n.B=-1;n.k=function(){Be.c.k.call(this);Ce(this)};n.uc=function(a){a.stopPropagation();Ce(this)};function Ce(a){if(a.q[a.B]){a.q[a.B].m();delete a.q[a.B]}var b=a.q[++a.B];if(b){b.ya(a);P(S(a),b,["nosupport","nosupportedsrc"],a.uc);a.sb?b.r(a.b()):b.va(a.b())}else a.dispatchEvent("nosupport")}n.d=function(){Be.c.d.call(this);for(var a=0;a<this.q.length;a++)this.q[a]&&this.q[a].m();delete this.q};n.C=function(){if(this.q[this.B])return this.q[this.B].C()};
 n.u=function(a,b){this.q[this.B]&&this.q[this.B].u(a,b);return this};n.O=function(){if(this.q[this.B])return this.q[this.B].O();return new A(0,0)};q("mcore.MultiPlayer",Be,void 0);function De(a,b,c,d){if(!t(a)||!t(b))e(Error("Start and end parameters must be arrays"));if(a.length!=b.length)e(Error("Start and end points must be the same length"));this.za=a;this.hc=b;this.duration=c;this.ub=d;this.coords=[]}z(De,Q);var Ee={},Fe=j;function Ge(){V.clearTimeout(Fe);var a=y(),b;for(b in Ee)He(Ee[b],a);Fe=Za(Ee)?j:V.setTimeout(Ge,20)}function Ie(a){a=w(a);delete Ee[a];if(Fe&&Za(Ee)){V.clearTimeout(Fe);Fe=j}}n=De.prototype;n.G=0;n.Hb=0;n.t=0;n.startTime=j;n.Eb=j;n.lb=j;
 n.play=function(a){if(a||this.G==0){this.t=0;this.coords=this.za}else if(this.G==1)return k;Ie(this);this.startTime=y();if(this.G==-1)this.startTime-=this.duration*this.t;this.Eb=this.startTime+this.duration;this.lb=this.startTime;this.t||this.ua();Je(this,"play");this.G==-1&&Je(this,"resume");this.G=1;a=w(this);a in Ee||(Ee[a]=this);Fe||(Fe=V.setTimeout(Ge,20));He(this,this.startTime);return i};n.stop=function(a){Ie(this);this.G=0;if(a)this.t=1;Ke(this,this.t);Je(this,"stop");this.ea()};
 n.d=function(){this.G!=0&&this.stop(k);Je(this,"destroy");De.c.d.call(this)};function He(a,b){a.t=(b-a.startTime)/(a.Eb-a.startTime);if(a.t>=1)a.t=1;a.Hb=1E3/(b-a.lb);a.lb=b;ga(a.ub)?Ke(a,a.ub(a.t)):Ke(a,a.t);if(a.t==1){a.G=0;Ie(a);Je(a,"finish");a.ea()}else a.G==1&&a.pb()}function Ke(a,b){a.coords=Array(a.za.length);for(var c=0;c<a.za.length;c++)a.coords[c]=(a.hc[c]-a.za[c])*b+a.za[c]}n.pb=function(){Je(this,"animate")};n.ua=function(){Je(this,"begin")};n.ea=function(){Je(this,"end")};

mediacore/public/scripts/mcore/players/controller.js
@@ -449,10 +449,10 @@ mcore.players.Controller.prototype.disposeInternal = function() {
 mcore.players.Controller.init = function(element, player, opt_callback) {
   element = goog.dom.getElement(element);
   var controller = new mcore.players.Controller(player);
+  if (opt_callback) { opt_callback(controller); }
   var decorateJob = function() {
     var decorate = function() {
       controller.decorate(element);
-      if (opt_callback) { opt_callback(controller); }
     }
     // Add a delay before the controller decorates the page in order to give
     // the UI thread an opportunity to take action elsewhere, reducing the

mediacore/templates/players/iframe.html
@@ -21,6 +21,7 @@
       xmlns:xi="http://www.w3.org/2001/XInclude">
 
 <head>
+	<title>${h.page_title(default=media.title, media=media)}</title>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 	<py:choose test="h.config['debug'] and bool(h.request.params.get('debug', False))">
 		<py:when test="True">
@@ -47,6 +48,12 @@
 </head>
 
 <body py:if="error" py:content="error" />
-<body py:if="not error" py:content="h.media_player(media, show_playerbar=False, js_init='function(ctrlr){ ctrlr.setFillScreen(true); }', **player_kwargs)" />
+<body py:if="not error">
+	${h.media_player(media, show_playerbar=False, js_init='function(ctrlr){ ctrlr.setFillScreen(true); }', **player_kwargs)}
+	<script type="text/javascript">
+		mcore.initPage();
+	</script>
+</body>
+
 
 </html>