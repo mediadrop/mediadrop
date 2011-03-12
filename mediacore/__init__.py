037a3e676cd647b89e1ea44c3ed962eb56270d0a
Nathan Wright <nathan@simplestation.com>
2011-03-11 21:06:23 -0800

    Remove unused import

--- mediacore/__init__.py
+++ mediacore/__init__.py
@@ -14,7 +14,6 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import sys
-from types import NoneType
 
 # Module description following the guidelines at:
 # http://bayes.colorado.edu/PythonGuidelines.html#module_formatting
