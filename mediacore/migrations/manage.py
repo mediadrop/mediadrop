mediadrop/mediadrop

commit 135feb80e75f92672c965ac829740c10582a5b6b

Update licensing information to describe new MIT license for front-end javascript.

mediacore/migrations/manage.py
@@ -1,3 +1,7 @@
+# This file is a part of MediaCore CE, Copyright 2009-2012 MediaCore Inc.
+# The source code contained in this file is licensed under the GPL.
+# See LICENSE.txt in the main project directory, for more information.
+
 #!/usr/bin/env python
 from migrate.versioning.shell import main
 main(debug='False')

commit 82421617cdef1ca6ccf00ed3041ec5d18b2c573a

move shebang back to the first line (regression from 135feb80e75f92672c965ac829740c10582a5b6b)

mediacore/migrations/manage.py
@@ -1,7 +1,7 @@
+#!/usr/bin/env python
 # This file is a part of MediaCore CE, Copyright 2009-2012 MediaCore Inc.
 # The source code contained in this file is licensed under the GPL.
 # See LICENSE.txt in the main project directory, for more information.
 
-#!/usr/bin/env python
 from migrate.versioning.shell import main
 main(debug='False')