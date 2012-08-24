5965cc8aed6f5599ea0a16154a280c05084bdd31
Felix Schwarz <felix.schwarz@oss.schwarz.eu>
2012-08-24 13:59:10 +0200

    add tests for JS delivery API

--- mediacore/lib/test/__init__.py
+++ mediacore/lib/test/__init__.py
@@ -11,9 +11,11 @@ import unittest
 
 def suite():
     from mediacore.plugin.tests import events_test
+    from mediacore.lib.tests import js_delivery_test
     
     suite = unittest.TestSuite()
     suite.addTest(events_test.suite())
+    suite.addTest(js_delivery_test.suite())
     return suite
 
 if __name__ == '__main__':
---
+++ mediacore/lib/tests/js_delivery_test.py
@@ -0,0 +1,103 @@
+# -*- coding: utf-8 -*-
+# This file is a part of MediaCore CE, Copyright 2009-2012 MediaCore Inc.
+# The source code in this file is dual licensed under the MIT license or the 
+# GPL version 3 or (at your option) any later version.
+# See LICENSE.txt in the main project directory, for more information.
+#
+# Copyright (c) 2012 Felix Schwarz <felix.schwarz@oss.schwarz.eu>
+
+from mediacore.lib.js_delivery import InlineJS, Script, Scripts
+from mediacore.lib.test.pythonic_testcase import *
+
+
+class ScriptTest(PythonicTestCase):
+    def test_can_tell_if_another_script_is_equal(self):
+        first = Script('/foo.js')
+        second = Script('/foo.js')
+        assert_equals(first, first)
+        assert_equals(first, second)
+        assert_equals(second, first)
+    
+    def test_can_tell_that_another_script_is_not_equal(self):
+        first = Script('/foo.js')
+        assert_not_equals(first, Script('/bar.js'))
+        assert_not_equals(first, None)
+
+
+class InlineJSTest(PythonicTestCase):
+    def test_can_tell_if_another_inlinescript_is_equal(self):
+        first = InlineJS('var a = 42;')
+        second = InlineJS('var a = 42;')
+        assert_equals(first, first)
+        assert_equals(first, second)
+        assert_equals(second, first)
+    
+    def test_can_tell_that_another_inlinescript_is_not_equal(self):
+        first = InlineJS('var a = 42;')
+        assert_not_equals(first, InlineJS('var a = null;'))
+        assert_not_equals(first, InlineJS('var a  =  null;'))
+        assert_not_equals(first, None)
+
+
+class ScriptsTest(PythonicTestCase):
+    # --- add scripts ----------------------------------------------------------
+    def test_can_add_a_script(self):
+        scripts = Scripts()
+        scripts.add(Script('/foo.js'))
+        assert_length(1, scripts)
+
+    def test_can_scripts_during_instantiation(self):
+        scripts = Scripts(Script('/foo.js'), Script('/bar.js'))
+        assert_length(2, scripts)
+
+    # --- duplicate handling ---------------------------------------------------
+    
+    def test_does_not_add_duplicate_scripts(self):
+        scripts = Scripts()
+        scripts.add(Script('/foo.js'))
+        scripts.add(Script('/foo.js'))
+        assert_length(1, scripts)
+        
+    def test_uses_non_async_if_conflicting_variants_are_added(self):
+        scripts = Scripts()
+        scripts.add(Script('/foo.js', async=True))
+        assert_length(1, scripts)
+        assert_true(scripts.scripts[0].async)
+        
+        scripts.add(Script('/foo.js'))
+        assert_length(1, scripts)
+        assert_false(scripts.scripts[0].async)
+    
+    # --- replacing scripts ----------------------------------------------------
+
+    def test_can_replace_script_with_key(self):
+        foo_script = Script('/foo.js', key='foo')
+        bar_script = Script('/bar.js', key='foo')
+        
+        scripts = Scripts()
+        scripts.add(foo_script)
+        scripts.replace_script_with_key(bar_script)
+        assert_length(1, scripts)
+        assert_contains(bar_script, scripts.scripts)
+    
+    # --- rendering ------------------------------------------------------------
+    def test_can_render_markup_for_all_scripts(self):
+        foo_script = Script('/foo.js')
+        bar_script = Script('/bar.js')
+        scripts = Scripts()
+        scripts.add(foo_script)
+        scripts.add(bar_script)
+        assert_equals(unicode(foo_script)+unicode(bar_script), scripts.render())
+
+
+
+import unittest
+def suite():
+    suite = unittest.TestSuite()
+    suite.addTest(unittest.makeSuite(ScriptTest))
+    suite.addTest(unittest.makeSuite(InlineJSTest))
+    suite.addTest(unittest.makeSuite(ScriptsTest))
+    return suite
+
+if __name__ == '__main__':
+    unittest.main(defaultTest='suite')
