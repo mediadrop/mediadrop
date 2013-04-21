58afe1ef32816f6cb00b6eaf1348b2689990c139
Felix Schwarz <felix.schwarz@oss.schwarz.eu>
2013-04-21 22:47:29 +0200

    remove MediaCore CE copyright header from htmlsanitizer. The (now removed) copyright header was obviously added by accident during the big "frontend JS MIT relicensing" in early 2012.
    The file contents are licensed under a "simplified BSD license"

--- mediacore/lib/xhtml/htmlsanitizer.py
+++ mediacore/lib/xhtml/htmlsanitizer.py
@@ -1,11 +1,4 @@
 # -*- coding: UTF-8 -*-
-# This file is a part of MediaCore CE (http://www.mediacorecommunity.org),
-# Copyright 2009-2013 MediaCore Inc., Felix Schwarz and other contributors.
-# For the exact contribution history, see the git revision log.
-# The source code contained in this file is licensed under the GPLv3 or
-# (at your option) any later version.
-# See LICENSE.txt in the main project directory, for more information.
-
 """
 Repository: https://code.launchpad.net/~python-scrapers/python-html-sanitizer/trunk
 Revision: r10
