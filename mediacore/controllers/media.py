mediadrop/mediadrop

commit 19dc7f880f5f874b630f18f76929ab73ce982e51

feed links are now added in controller functions to reduce the crazy Genshi py:match stuff

mediacore/controllers/media.py
@@ -129,6 +129,15 @@ class MediaController(BaseController):
 
         latest = viewable_media(latest.exclude(featured))[:8]
         popular = viewable_media(popular.exclude(featured, latest))[:5]
+        if request.settings['sitemaps_display'] == 'True':
+            response.feed_links.extend([
+                (url_for(controller='/sitemaps', action='google'), u'Sitemap XML'),
+                (url_for(controller='/sitemaps', action='mrss'), u'Sitemap RSS'),
+            ])
+        if request.settings['rss_display'] == 'True':
+            response.feed_links.extend([
+                (url_for(controller='/sitemaps', action='latest'), u'Latest RSS'),
+            ])
 
         return dict(
             featured = featured,

mediacore/controllers/categories.py
@@ -12,7 +12,7 @@ from sqlalchemy import orm
 from mediacore.lib.base import BaseController
 from mediacore.lib.decorators import (beaker_cache, expose, observable, 
     paginate, validate)
-from mediacore.lib.helpers import content_type_for_response, viewable_media
+from mediacore.lib.helpers import content_type_for_response, url_for, viewable_media
 from mediacore.model import Category, Media, fetch_row
 from mediacore.plugin import events
 from mediacore.validation import LimitFeedItemsValidator
@@ -60,6 +60,11 @@ class CategoriesController(BaseController):
 
         if c.category:
             media = media.in_category(c.category)
+            
+            response.feed_links.append((
+                url_for(controller='/categories', action='feed', slug=c.category.slug),
+                _('Latest media in %s') % c.category.name
+            ))
 
         latest = media.order_by(Media.publish_on.desc())
         popular = media.order_by(Media.popularity_points.desc())

mediacore/controllers/podcasts.py
@@ -12,7 +12,7 @@ from mediacore.lib import helpers
 from mediacore.lib.base import BaseController
 from mediacore.lib.decorators import (beaker_cache, expose, observable, 
     paginate, validate)
-from mediacore.lib.helpers import content_type_for_response, redirect
+from mediacore.lib.helpers import content_type_for_response, url_for, redirect
 from mediacore.model import Media, Podcast, fetch_row
 from mediacore.plugin import events
 from mediacore.validation import LimitFeedItemsValidator
@@ -84,6 +84,12 @@ class PodcastsController(BaseController):
         episodes, show = helpers.filter_library_controls(episodes, show)
 
         episodes = viewable_media(episodes)
+        
+        if request.settings['rss_display'] == 'True':
+            response.feed_links.append(
+               (url_for(action='feed'), podcast.title)
+            )
+
         return dict(
             podcast = podcast,
             episodes = episodes,

mediacore/lib/base.py
@@ -78,6 +78,7 @@ class BareBonesController(WSGIController):
         self.setup_translator()
         response.scripts = Scripts()
         response.stylesheets = StyleSheets()
+        response.feed_links = []
         response.facebook = None
         request.perm = request.environ['mediacore.perm']
 

mediacore/templates/categories/index.html
@@ -20,9 +20,6 @@ See LICENSE.txt in the main project directory, for more information.
 	<meta name="description" content="${h.meta_description(category=c.category or 'all')}" />
 	<meta py:strip="h.meta_robots_noindex(category=c.category or 'all')" name="robots" content="noindex,follow" />
 	<link href="${h.url_for('/styles/categories.css')}" media="screen" rel="stylesheet" type="text/css" />
-	<link py:if="c.category"
-	      rel="alternate" type="application/rss+xml" title="${_('Latest media in %s') % c.category.name}"
-	      href="${h.url_for(controller='/categories', action='feed', slug=c.category.slug)}" />
 </head>
 <body>
 	<div py:if="latest" id="category-latest" class="${popular and 's-grid-column' or None}" py:with="latest_url = c.category and h.url_for(action='more', order='latest') or h.url_for(controller='/media', show='latest')">

mediacore/templates/layout.html
@@ -14,17 +14,6 @@ See LICENSE.txt in the main project directory, for more information.
       xmlns:xi="http://www.w3.org/2001/XInclude"
       py:strip="">
 
-<?python feed_links = [] ?>
-
-<head py:match="head">
-	<link py:match="link" py:attrs="select('@*')"><?python
-		attrs = h.attrs_to_dict(select('@*'))
-		if attrs.get('type') == 'application/rss+xml':
-			feed_links.append((attrs['href'], attrs['title']))
-	?></link>
-	<meta py:replace="select('*|text()')"/>
-</head>
-
 <body py:match="body" py:attrs="select('@*')">
 	${Markup(settings['appearance_custom_header_html'])}
 
@@ -42,7 +31,7 @@ See LICENSE.txt in the main project directory, for more information.
 		<div id="mcore-feeds" class="f-lft">
 			<h5>Feeds:</h5>
 			<ul>
-				<li py:for="href, title in feed_links"><a href="${href}" class="mcore-foot-link underline-hover">${title}</a></li>
+				<li py:for="href, title in response.feed_links"><a href="${href}" class="mcore-foot-link underline-hover">${title}</a></li>
 			</ul>
 		</div>
 		<div py:if="settings['appearance_display_mediacore_credits']" id="mcore-credits" class="f-rgt">

mediacore/templates/master.html
@@ -35,6 +35,10 @@ See LICENSE.txt in the main project directory, for more information.
 	<py:if test="settings['appearance_custom_head_tags']">
 		${Markup(settings['appearance_custom_head_tags'])}
 	</py:if>
+	
+	<py:for each="url, title in response.feed_links">
+		<link type="application/rss+xml" rel="alternate" href="${url}" title="${title}" />
+	</py:for>
 	<meta py:replace="select('*')"/>
 	<script py:if="settings.get('google_analytics_uacct', None)" type="text/javascript">
 		var _gaq = _gaq || [];

mediacore/templates/media/explore.html
@@ -19,15 +19,6 @@ See LICENSE.txt in the main project directory, for more information.
 	<title>${h.page_title(default='Explore', media='all')}</title>
 	<meta name="keywords" content="${h.meta_keywords(media='all') or 'MediaCore'}" />
 	<meta name="description" content="${h.meta_description(media='all') or 'MediaCore Video CMS'}" />
-	<link py:if="settings['sitemaps_display'] == 'True'"
-	      rel="alternate" type="application/rss+xml" title="Sitemap XML"
-	      href="${h.url_for(controller='/sitemaps', action='google')}" />
-	<link py:if="settings['sitemaps_display'] == 'True'"
-	      rel="alternate" type="application/rss+xml" title="Sitemap RSS"
-	      href="${h.url_for(controller='/sitemaps', action='mrss')}" />
-	<link py:if="settings['rss_display'] == 'True'"
-	      rel="alternate" type="application/rss+xml" title="Latest RSS"
-	      href="${h.url_for(controller='/sitemaps', action='latest')}" />
 </head>
 <body class="nav-explore-on">
 	<div class="mediacore-content clearfix">

commit fb0e5adaa5d672e3523f7c05fe461b7c143736f6

mark feed links titles as translatable (regression from 19dc7f880f5f874b630f18f76929ab73ce982e51 - when this was in the template these strings were automatically extracted by Genshi's extractor)

mediacore/controllers/media.py
@@ -86,11 +86,11 @@ class MediaController(BaseController):
         if (request.settings['rss_display'] == 'True') and (not (q or tag)):
             if show == 'latest':
                 response.feed_links.extend([
-                    (url_for(controller='/sitemaps', action='latest'), u'Latest RSS'),
+                    (url_for(controller='/sitemaps', action='latest'), _(u'Latest RSS')),
                 ])
             elif show == 'featured':
                 response.feed_links.extend([
-                    (url_for(controller='/sitemaps', action='featured'), u'Featured RSS'),
+                    (url_for(controller='/sitemaps', action='featured'), _(u'Featured RSS')),
                 ])
 
         media = viewable_media(media)
@@ -141,12 +141,12 @@ class MediaController(BaseController):
         popular = viewable_media(popular.exclude(featured, latest))[:5]
         if request.settings['sitemaps_display'] == 'True':
             response.feed_links.extend([
-                (url_for(controller='/sitemaps', action='google'), u'Sitemap XML'),
-                (url_for(controller='/sitemaps', action='mrss'), u'Sitemap RSS'),
+                (url_for(controller='/sitemaps', action='google'), _(u'Sitemap XML')),
+                (url_for(controller='/sitemaps', action='mrss'), _(u'Sitemap RSS')),
             ])
         if request.settings['rss_display'] == 'True':
             response.feed_links.extend([
-                (url_for(controller='/sitemaps', action='latest'), u'Latest RSS'),
+                (url_for(controller='/sitemaps', action='latest'), _(u'Latest RSS')),
             ])
 
         return dict(