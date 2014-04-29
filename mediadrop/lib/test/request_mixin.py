bca15525f5480c365a3452d3d40bb9c2c4dfe193
Felix Schwarz <felix.schwarz@oss.schwarz.eu>
2014-04-29 10:52:33 +0200

    fix spelling in dual licensing header comment

--- mediadrop/lib/test/request_mixin.py
+++ mediadrop/lib/test/request_mixin.py
@@ -2,7 +2,7 @@
 # This file is a part of MediaDrop (http://www.mediadrop.net),
 # Copyright 2009-2013 MediaDrop contributors
 # For the exact contribution history, see the git revision log.
-# The source code in this file is is dual licensed under the MIT license or
+# The source code in this file is dual licensed under the MIT license or
 # the GPLv3 or (at your option) any later version.
 # See LICENSE.txt in the main project directory, for more information.
 
