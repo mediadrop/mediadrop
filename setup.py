8e91ef033f6bbea12046216e1a52903446c05eea
Felix Schwarz <felix.schwarz@oss.schwarz.eu>
2018-04-10 22:22:28 +0200

    travis: fix build with Python 2.6

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ def requires_from_file(filename):
     requirements = []
     with open(filename, 'r') as requirements_fp:
         for line in requirements_fp.readlines():
-            match = re.search('^\s*([a-zA-Z][^#]+?)(\s*#.+)?\n$', line)
+            match = re.search('^\s*([a-zA-Z][^#]+?)(\s*(#.+)|(;.+))?\n$', line)
             if match:
                 requirements.append(match.group(1))
     return requirements
